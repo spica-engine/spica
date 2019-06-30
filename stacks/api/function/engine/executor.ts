31b22aff49cebe9588bde191607a586631a95cca
thesayyn <thesayyn@gmail.com>
2019-06-30 14:29:42 +0300

    feat(function): initial work for isolated function executor

--- stacks/api/function/engine/executor.ts
+++ stacks/api/function/engine/executor.ts
@@ -1,84 +1,97 @@
-import {Execution} from "./interface";
-import * as ts from "typescript";
-import {NodeVM} from "vm2";
-import * as path from "path";
+import * as ivm from 'isolated-vm';
+import ts = require('typescript/lib/tsserverlibrary');
+import { Execution } from './interface';
 
 export abstract class FunctionExecutor {
   abstract execute(execution: Execution): Promise<any>;
 }
 
-export class VM2Executor extends FunctionExecutor {
-  execute(execution: Execution): Promise<any> {
-    return new Promise(resolve => {
-      const vm = new NodeVM({
-        timeout: execution.timeout || 100,
-        sandbox: {console: execution.logger, exports: {}},
-        require: {
-          mock: execution.modules,
-          external: true,
-          builtin: ["*"]
-        },
-        compiler: (code: string) => {
-          return ts.transpileModule(code, {
-            compilerOptions: {
-              module: ts.ModuleKind.CommonJS,
-              target: ts.ScriptTarget.ES2018
-            }
-          }).outputText;
-        }
-      });
+export class IsolatedVMExecutor extends FunctionExecutor {
+  async execute(execution: Execution): Promise<any> {
+    const code = ts.transpileModule(execution.script, {
+      compilerOptions: {
+        module: ts.ModuleKind.CommonJS,
+        target: ts.ScriptTarget.ES2018,
+        noImplicitUseStrict: false
+      }
+    }).outputText;
 
-      const run = vm.run(execution.script, path.join(execution.cwd, "index.ts"));
-      resolve(run[execution.target.handler](...execution.parameters));
-    }).catch(error => {
-      // We have to catch any errors to forward
-      // it to logger manually since the vm2
-      // Cannot catch async error that occurs in
-      // async contexts
-      execution.logger.error(error);
-      return Promise.reject(error);
-    });
+    const vm = new ivm.Isolate({ memoryLimit: execution.memoryLimit });
+    let context = vm.createContextSync();
+    let jail = context.global;
+    jail.setSync('global', jail.derefInto());
+    jail.setSync('ivm', ivm);
+
+    jail.setSync('console', this.makeTransferable(execution.logger));
+
+    vm.compileScriptSync(`
+    function resolve(val) {
+      if ( val instanceof ivm.Reference ) {
+        switch (val.typeof) {
+          case 'function':
+            return function (...args) { 
+              val.applyIgnored(undefined, args.map(arg => new ivm.ExternalCopy(arg).copyInto()))
+            }
+          case 'object':
+            const actualValue = val.copySync();
+            return Object.keys(actualValue).reduce( (accumulator, key) => {
+              accumulator[key] = resolve(actualValue[key]); 
+              return accumulator;
+            }, {});
+          default:
+            return val.copySync();
+        }
+      } else if ( val instanceof ivm.ExternalCopy ) {
+        return val.copy();
+      } else {
+        return val;
+      }
+    }`).runSync(context);
+
+    vm.compileScriptSync(
+      `var exports = {};
+      global.console = resolve(global.console);
+      `,
+      { filename: 'logger.spica.internal' }
+    ).runSync(context);
+
+    vm.compileScriptSync(code, { filename: `function@${execution.target.id}.js` }).runSync(context);
+
+
+    vm.compileScriptSync(
+      `function spica_internal(...params) {
+        return exports.${execution.target.handler}(...params.map((p) => resolve(p)));
+      }`,
+      { filename: `bootstrap.spica.internal` }
+    ).runSync(context);
+
+
+    const fn = context.global.getSync(`spica_internal`);
+    return fn.apply(undefined, execution.parameters.map(param => this.makeTransferable(param)), { timeout: execution.timeout })
+      .then(() => {
+        console.log(vm.cpuTime, vm.wallTime, vm.referenceCount, vm.getHeapStatisticsSync());
+        if (!vm.isDisposed) {
+          vm.dispose();
+        }
+      }).catch(error => {
+        if (!vm.isDisposed) {
+          vm.dispose();
+        }
+        return Promise.reject(error);
+      })
+  }
+
+  private makeTransferable(value: any): ivm.Transferable {
+    switch (typeof value) {
+      case 'function':
+        return new ivm.Reference((...args) => value(...args));
+      case 'object':
+        return !value ? value : Object.getOwnPropertyNames(value).filter(k => !k.startsWith('_')).reduce((accumulator, key) => {
+          accumulator.setSync(key, this.makeTransferable(value[key]));
+          return accumulator;
+        }, new ivm.Reference(value));
+      default:
+        return new ivm.ExternalCopy(value);
+    }
   }
 }
-
-/*
-import * as ivm from 'isolated-vm';
-export class IsolatedVMExecutor extends FunctionExecutor {
-	execute(execution: Execution, fn: FFunction) {
-		return new Promise(() => {
-			const isolate = new ivm.Isolate({ memoryLimit: fn.memoryLimit || 128, inspector: true });
-			const context = isolate.createContextSync();
-			const jail = context.global;
-			jail.setSync('global', jail.derefInto());
-			jail.setSync('_ivm', ivm);
-			jail.setSync(
-				'_log',
-				new ivm.Reference(function(...args) {
-					console.log(...args, 'annen');
-				})
-			);
-			const utilityScript = isolate.compileScriptSync(`new  function() {
-                let ivm = _ivm;
-                delete _ivm;
-
-                let log = _log;
-                delete _log;
-                global.console.log = function(...args) {
-                    log(undefined, args.map(arg => new ivm.ExternalCopy(arg).copyInto()));
-                };
-            }`);
-			const transpiled = ts.transpileModule(fn.source, {
-				compilerOptions: {
-					module: ts.ModuleKind.CommonJS,
-					target: ts.ScriptTarget.Latest,
-					noImplicitUseStrict: true
-				}
-			}).outputText;
-			const script = isolate.compileScriptSync(transpiled);
-			return Promise.all([
-				utilityScript.runSync(context),
-				script.run(context, { timeout: fn.timeout || 20000 })
-			]).then((d) => console.log(d));
-		});
-	}
-}*/
