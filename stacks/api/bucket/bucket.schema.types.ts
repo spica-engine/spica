16898b95b3096652caec85b8cdf9b9a014a107f3
thesayyn <thesayyn@gmail.com>
2019-07-01 11:22:42 +0300

    refactor(bucket): remove redundant formats and defaults

--- stacks/api/bucket/bucket.schema.types.ts
+++ stacks/api/bucket/bucket.schema.types.ts
@@ -1,36 +1,4 @@
-import {Default, Format, Keyword} from "@spica-server/core/schema";
-import {ObjectId} from "@spica-server/database";
-
-export const CREATED_AT: Default = {
-  keyword: ":created_at",
-  type: "date",
-  create: data => {
-    return data || new Date().toISOString();
-  }
-};
-
-export const UPDATED_AT: Default = {
-  keyword: ":updated_at",
-  type: "date",
-  create: () => {
-    return new Date().toISOString();
-  }
-};
-
-export const BUCKET_ID: Format = {
-  name: "bucketid",
-  type: "string",
-  coerce: bucketId => {
-    return new ObjectId(bucketId);
-  },
-  validate: bucketId => {
-    try {
-      return !!bucketId && !!new ObjectId(bucketId);
-    } catch {
-      return false;
-    }
-  }
-};
+import {Keyword} from "@spica-server/core/schema";
 
 export const CUSTOM_TYPES: Keyword = {
   name: "type",
