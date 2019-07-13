254434de05a1d995d21cec5cbdd0374f4a5bdc2e
thesayyn <thesayyn@gmail.com>
2019-07-13 11:23:33 +0300

    wip: cleanup

--- docs/site/src/app/pages/docs/docs.component.ts
+++ docs/site/src/app/pages/docs/docs.component.ts
@@ -5,7 +5,7 @@ import {DocService} from "../../services/doc.service";
 @Component({
   selector: "app-doc",
   templateUrl: "./docs.component.html",
-  styleUrls: ["./docs.component.css"]
+  styleUrls: ["./docs.component.scss"]
 })
 export class DocsComponent implements OnInit {
   $apiDocs: Observable<any>;
