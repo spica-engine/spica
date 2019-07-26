e23e92886f76a5daec00d254962b71982bdeb9ff
thesayyn <thesayyn@gmail.com>
2019-07-26 23:10:53 +0300

    test(database): add support for replset spec

--- packages/database/testing/testing.module.ts
+++ packages/database/testing/testing.module.ts
@@ -1,11 +1,16 @@
-import {DynamicModule, Module, OnModuleDestroy, Global} from "@nestjs/common";
+import {DynamicModule, Global, Module, OnModuleDestroy} from "@nestjs/common";
 import {ModuleRef} from "@nestjs/core";
 import {DatabaseService, MongoClient} from "@spica-server/database";
-import {MongoMemoryServer} from "mongodb-memory-server-core";
-import * as path from "path";
 import * as fs from "fs";
+import {MongoMemoryReplSet, MongoMemoryServer} from "mongodb-memory-server-core";
+import * as path from "path";
 
 let installing: Promise<any> = Promise.resolve();
+
+const downloadDir = path.join(
+  path.dirname(fs.realpathSync(require.resolve("mongodb-memory-server-core"))),
+  ".cache"
+);
 
 @Global()
 @Module({})
@@ -13,10 +18,6 @@ export class DatabaseTestingModule implements OnModuleDestroy {
   constructor(private moduleRef: ModuleRef) {}
 
   static create(): DynamicModule {
-    const downloadDir = path.join(
-      path.dirname(fs.realpathSync(require.resolve("mongodb-memory-server-core"))),
-      ".cache"
-    );
     return {
       module: DatabaseTestingModule,
       providers: [
@@ -56,7 +57,59 @@ export class DatabaseTestingModule implements OnModuleDestroy {
     };
   }
 
+  static replicaSet(): DynamicModule {
+    return {
+      module: DatabaseTestingModule,
+      providers: [
+        {
+          provide: MongoMemoryReplSet,
+          useFactory: async () =>
+            new MongoMemoryReplSet({
+              binary: {
+                downloadDir,
+                version: "4.0.3"
+              },
+              instanceOpts: [
+                {storageEngine: "wiredTiger"},
+                {storageEngine: "wiredTiger"},
+                {storageEngine: "wiredTiger"}
+              ]
+            })
+        },
+        {
+          provide: MongoClient,
+          useFactory: async (server: MongoMemoryReplSet) => {
+            await server.waitUntilRunning();
+            const connectionString = await server.getConnectionString();
+            // Issue: https://github.com/nodkz/mongodb-memory-server/issues/166
+            await new Promise(resolve => setTimeout(resolve, 2000));
+            return MongoClient.connect(
+              `${connectionString}?replicaSet=${server.opts.replSet.name}`,
+              {
+                useNewUrlParser: true,
+                replicaSet: server.opts.replSet.name
+              }
+            );
+          },
+          inject: [MongoMemoryReplSet]
+        },
+        {
+          provide: DatabaseService,
+          useFactory: async (client: MongoClient, server: MongoMemoryReplSet) =>
+            client.db(await server.getDbName()),
+          inject: [MongoClient, MongoMemoryReplSet]
+        }
+      ],
+      exports: [DatabaseService]
+    };
+  }
+
   onModuleDestroy() {
-    this.moduleRef.get(MongoMemoryServer).stop();
+    try {
+      this.moduleRef.get(MongoMemoryServer).stop();
+    } catch {}
+    try {
+      this.moduleRef.get(MongoMemoryReplSet).stop();
+    } catch {}
   }
 }
