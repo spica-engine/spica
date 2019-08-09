29437b454ab127ec09c19f30fb315ea2eed57d4f
thesayyn <thesayyn@gmail.com>
2019-08-09 12:17:50 +0300

    layout fixes

--- docs/site/src/polyfills.ts
+++ docs/site/src/polyfills.ts
@@ -55,8 +55,13 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import "zone.js/dist/zone"; // Included with Angular CLI.
-
+// Needed for browsers without native `customElements`.
+// (E.g.: Edge, Firefox, IE, Safari)
+import "@webcomponents/custom-elements/custom-elements.min";
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+// Needed on browsers with native `customElements`.
+// (E.g.: Chrome, Opera)
+import "@webcomponents/custom-elements/src/native-shim";
+import "zone.js/dist/zone"; // Included with Angular CLI.
