bc10e3408c22ce925bf30e315aa2ca7f8f573b99
thesayyn <thesayyn@gmail.com>
2019-09-12 21:34:38 +0300

    refactor: take users scheme preference

--- stacks/spica/packages/core/src/layout/theme-switcher/theme-switcher.component.ts
+++ stacks/spica/packages/core/src/layout/theme-switcher/theme-switcher.component.ts
@@ -1,17 +1,37 @@
-import {Component, Renderer2} from "@angular/core";
+import {BreakpointObserver} from "@angular/cdk/layout";
+import {DOCUMENT} from "@angular/common";
+import {Component, Inject, OnDestroy, Renderer2} from "@angular/core";
+import {Subscription} from "rxjs";
+
 @Component({
   selector: "theme-switcher",
   templateUrl: "./theme-switcher.component.html"
 })
-export class ThemeSwitcherComponent {
-  theme: boolean = false;
-  constructor(private renderer: Renderer2) {}
+export class ThemeSwitcherComponent implements OnDestroy {
+  isDark: boolean = false;
 
-  themeChange() {
-    if (this.theme) {
-      this.renderer.addClass(document.body, "dark");
+  private mediaMatchObserver: Subscription;
+
+  constructor(
+    @Inject(DOCUMENT) private document: any,
+    private renderer: Renderer2,
+    breakpointObserver: BreakpointObserver
+  ) {
+    this.mediaMatchObserver = breakpointObserver
+      .observe("(prefers-color-scheme: dark)")
+      .subscribe(r => this.changeScheme(r.matches));
+  }
+
+  changeScheme(isDark: boolean) {
+    this.isDark = isDark;
+    if (this.isDark) {
+      this.renderer.addClass(this.document.body, "dark");
     } else {
-      this.renderer.removeClass(document.body, "dark");
+      this.renderer.removeClass(this.document.body, "dark");
     }
+  }
+
+  ngOnDestroy(): void {
+    this.mediaMatchObserver.unsubscribe();
   }
 }
