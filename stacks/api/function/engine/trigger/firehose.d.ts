24900b58107a49e01df1093d9248c9c5b56191f4
thesayyn <thesayyn@gmail.com>
2019-10-16 18:41:45 +0300

    refactor(function): use more appropriate prebuilt event names

--- stacks/api/function/engine/trigger/firehose.d.ts
+++ stacks/api/function/engine/trigger/firehose.d.ts
@@ -10,9 +10,19 @@ declare namespace triggers.firehose {
   export interface Client {
     readonly remoteAddress: string;
     send(event: string, data: any): void;
+    close(): void;
   }
   export interface Event<T = any> {
     name: string;
     data: T;
+  }
+  export interface ConnectionEvent {
+    name: string;
+    request: {
+      url: string;
+      headers: {
+        [h: string]: string;
+      };
+    };
   }
 }
