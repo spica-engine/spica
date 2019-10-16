0d7ec12968524f48d3429dd272aed0992e9bbd7d
thesayyn <thesayyn@gmail.com>
2019-10-16 18:39:18 +0300

    test(core): refactor ws client

--- packages/core/testing/websocket.ts
+++ packages/core/testing/websocket.ts
@@ -1,5 +1,5 @@
 import {Injectable} from "@nestjs/common";
-import * as Client from "ws";
+import * as CL from "ws";
 
 @Injectable()
 export class Websocket {
@@ -12,4 +12,28 @@ export class Websocket {
   }
 }
 
-export type Client = Client;
+export class Client extends CL {
+  get connect() {
+    return new Promise(resolve => this.once("open", (...args) => setTimeout(resolve, 2, args)));
+  }
+
+  get() {
+    return new Promise(resolve => this.once("open", (...args) => setTimeout(resolve, 2, args)));
+  }
+
+  close() {
+    super.close();
+    return new Promise(resolve => this.once("close", (...args) => setTimeout(resolve, 2, args)));
+  }
+
+  send(data: any) {
+    return new Promise((resolve, reject) =>
+      super.send(data, err => {
+        if (err) {
+          return reject(err);
+        }
+        setTimeout(resolve, 1);
+      })
+    );
+  }
+}
