9f66ba5da2faae32b22949907b46c5ab7eed9dfb
thesayyn <thesayyn@gmail.com>
2019-10-21 14:47:13 +0300

    remove all tests from modules

--- stacks/spica/src/storage/components/picker/picker.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import {PickerDirective} from "./picker.directive";
-
-describe("PickerDirective", () => {
-  it("should create an instance", () => {
-    const directive = new PickerDirective(null);
-    expect(directive).toBeTruthy();
-  });
-});
