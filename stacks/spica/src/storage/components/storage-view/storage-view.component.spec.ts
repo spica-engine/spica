5749ffcf0b0008e3b14c5d93cea6a625f3933833
thesayyn <thesayyn@gmail.com>
2019-11-08 13:44:02 +0300

    latest

---
+++ stacks/spica/src/storage/components/storage-view/storage-view.component.spec.ts
@@ -0,0 +1,254 @@
+import {HttpResponse} from "@angular/common/http";
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {Component, DebugElement} from "@angular/core";
+import {ComponentFixture, TestBed} from "@angular/core/testing";
+import {MatIconModule} from "@angular/material";
+import {By} from "@angular/platform-browser";
+import {empty, of} from "rxjs";
+import {StorageViewComponent} from "./storage-view.component";
+
+@Component({
+  template: `
+    <storage-view [blob]="blob"></storage-view>
+  `
+})
+class TestCmp {
+  blob: any;
+}
+
+function base64toBlob(data: string, type: string) {
+  const encodedString = atob(data);
+  const arrayBuffer = new ArrayBuffer(encodedString.length);
+  const uintArray = new Uint8Array(arrayBuffer);
+
+  for (let i = 0; i < encodedString.length; i++) {
+    uintArray[i] = encodedString.charCodeAt(i);
+  }
+  return new Blob([arrayBuffer], {type});
+}
+
+fdescribe("StorageViewComponent", () => {
+  let fixture: ComponentFixture<TestCmp>;
+  let viewComponent: Omit<DebugElement, "componentInstance"> & {
+    componentInstance: StorageViewComponent;
+  };
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, MatIconModule],
+      declarations: [StorageViewComponent, TestCmp]
+    });
+
+    fixture = TestBed.createComponent(TestCmp);
+
+    viewComponent = fixture.debugElement.query(By.directive(StorageViewComponent));
+  });
+
+  describe("with url", () => {
+    let httpTestingController: HttpTestingController;
+    let observeSpy: jasmine.Spy<any>;
+
+    beforeEach(() => {
+      httpTestingController = TestBed.get(HttpTestingController);
+      observeSpy = spyOn(
+        fixture.debugElement.query(By.directive(StorageViewComponent)).componentInstance,
+        "observe"
+      ).and.returnValue(of(true));
+    });
+
+    it("should handle loading errors from http", () => {
+      fixture.componentInstance.blob = "http://example/test.png";
+      fixture.detectChanges();
+
+      const req = httpTestingController.expectOne("http://example/test.png");
+      req.error(new ErrorEvent("Network error"));
+      fixture.detectChanges();
+
+      expect(viewComponent.query(By.css("div mat-icon")).nativeElement.textContent).toBe("error");
+      expect(viewComponent.query(By.css("div h3")).nativeElement.textContent).toBe(
+        "Could not load the object."
+      );
+      expect(viewComponent.query(By.css("div small")).nativeElement.textContent).toBe(
+        "Network error"
+      );
+
+      fixture.componentInstance.blob = "http://example/test1.png";
+    });
+
+    it("should reset previous error state", () => {
+      fixture.componentInstance.blob = "http://example/test.png";
+      fixture.detectChanges();
+
+      const req = httpTestingController.expectOne("http://example/test.png");
+      req.error(new ErrorEvent("Network error"));
+      expect(viewComponent.componentInstance.error).toBeTruthy();
+
+      fixture.componentInstance.blob = "http://example/test1.png";
+      fixture.detectChanges();
+      expect(viewComponent.componentInstance.error).not.toBeTruthy();
+    });
+
+    it("should not defer loading", () => {
+      fixture.componentInstance.blob = "http://example/test.png";
+      fixture.detectChanges();
+
+      expect(observeSpy).toHaveBeenCalledTimes(1);
+      httpTestingController.expectOne("http://example/test.png");
+    });
+
+    it("should defer loading", () => {
+      observeSpy.and.returnValue(empty());
+      fixture.componentInstance.blob = "http://example/test.png";
+      fixture.detectChanges();
+
+      expect(observeSpy).toHaveBeenCalledTimes(1);
+      httpTestingController.verify();
+    });
+
+    it("should show images", done => {
+      fixture.componentInstance.blob = "http://example/test.png";
+      fixture.detectChanges();
+
+      expect(viewComponent.componentInstance.ready).toBe(false);
+
+      const req = httpTestingController.expectOne("http://example/test.png");
+      req.flush(
+        base64toBlob(
+          "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII=",
+          "image/png"
+        )
+      );
+      fixture.detectChanges();
+
+      expect(viewComponent.componentInstance.ready).toBe(false);
+      expect(viewComponent.componentInstance.contentType).toBe("image/png");
+      expect(viewComponent.query(By.css("img")).nativeElement.src).toBeTruthy();
+
+      setTimeout(() => {
+        expect(viewComponent.componentInstance.ready).toBe(true);
+        done();
+      }, 10);
+    });
+
+    it("should show videos", () => {
+      fixture.componentInstance.blob = "http://example/test.mp4";
+      fixture.detectChanges();
+
+      expect(viewComponent.componentInstance.ready).toBe(false);
+
+      const req = httpTestingController.expectOne("http://example/test.mp4");
+      req.event(
+        new HttpResponse({
+          body: new Blob([], {type: "video/mp4"}),
+          status: 200,
+          statusText: "OK"
+        })
+      );
+      fixture.detectChanges();
+
+      expect(viewComponent.componentInstance.contentType).toBe("video/mp4");
+      expect(viewComponent.query(By.css("video source")).nativeElement.src).toBeTruthy();
+      expect(viewComponent.componentInstance.ready).toBe(true);
+    });
+
+    it("should not show other types", () => {
+      fixture.componentInstance.blob = "http://example/test.json";
+      fixture.detectChanges();
+      expect(viewComponent.componentInstance.ready).toBe(false);
+
+      const req = httpTestingController.expectOne("http://example/test.json");
+      req.event(
+        new HttpResponse({
+          body: new Blob([], {type: "application/json"}),
+          status: 200,
+          statusText: "OK"
+        })
+      );
+      fixture.detectChanges();
+
+      expect(viewComponent.componentInstance.contentType).toBe("application/json");
+      expect(viewComponent.componentInstance.ready).toBe(true);
+
+      expect(viewComponent.query(By.css("mat-icon")).nativeElement.textContent).toBe("warning");
+      expect(viewComponent.query(By.css("h3")).nativeElement.textContent).toBe(
+        "This object cannot be viewed."
+      );
+    });
+  });
+
+  describe("with blob", () => {
+    it("should show images", done => {
+      expect(viewComponent.componentInstance.ready).toBe(false);
+
+      fixture.componentInstance.blob = base64toBlob(
+        "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII=",
+        "image/png"
+      );
+      fixture.detectChanges();
+
+      expect(viewComponent.componentInstance.ready).toBe(false);
+      expect(viewComponent.componentInstance.contentType).toBe("image/png");
+      setTimeout(() => {
+        expect(viewComponent.componentInstance.ready).toBe(true);
+        done();
+      }, 10);
+    });
+
+    it("should show videos", () => {
+      fixture.componentInstance.blob = new Blob(["1"], {type: "video/mp4"});
+      fixture.detectChanges();
+
+      expect(viewComponent.componentInstance.contentType).toBe("video/mp4");
+      expect(viewComponent.componentInstance.ready).toBe(false);
+    });
+
+    it("should not show other types", () => {
+      fixture.componentInstance.blob = new Blob([], {type: "application/json"});
+      fixture.detectChanges();
+
+      expect(viewComponent.componentInstance.contentType).toBe("application/json");
+      expect(viewComponent.componentInstance.ready).toBe(true);
+    });
+  });
+
+  describe("with storage object", () => {
+    it("should show images", () => {
+      fixture.componentInstance.blob = {
+        url: "http://example/test.png",
+        content: {
+          type: "image/png"
+        }
+      };
+      fixture.detectChanges();
+
+      expect(viewComponent.componentInstance.contentType).toBe("image/png");
+      expect(viewComponent.componentInstance.ready).toBe(false);
+    });
+
+    it("should show videos", () => {
+      fixture.componentInstance.blob = {
+        url: "http://example/test.mp4",
+        content: {
+          type: "video/mp4"
+        }
+      };
+      fixture.detectChanges();
+
+      expect(viewComponent.componentInstance.contentType).toBe("video/mp4");
+      expect(viewComponent.componentInstance.ready).toBe(false);
+    });
+
+    it("should not show other types", () => {
+      fixture.componentInstance.blob = {
+        url: "http://example/test.json",
+        content: {
+          type: "application/json"
+        }
+      };
+      fixture.detectChanges();
+
+      expect(viewComponent.componentInstance.contentType).toBe("application/json");
+      expect(viewComponent.componentInstance.ready).toBe(true);
+    });
+  });
+});
---
+++ stacks/spica/src/storage/components/storage/storage.component.spec.ts
@@ -0,0 +1,253 @@
+import {HttpEventType, HttpResponse} from "@angular/common/http";
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import {Component, Directive, EventEmitter, Input, Output} from "@angular/core";
+import {ComponentFixture, TestBed} from "@angular/core/testing";
+import {
+  MatIconModule,
+  MatProgressBarModule,
+  MatProgressSpinner,
+  MatProgressSpinnerModule,
+  MatTooltipModule
+} from "@angular/material";
+import {By} from "@angular/platform-browser";
+import {INPUT_SCHEMA} from "@spica-client/common";
+import {StorageComponent} from "./storage.component";
+
+@Directive({
+  selector: "[storagePicker]"
+})
+class PickerDirective {
+  @Input("storagePicker") options: any;
+  @Output("ngModelChange") change = new EventEmitter();
+}
+
+@Component({
+  selector: "storage-view",
+  template: ""
+})
+class StorageViewCmp {
+  @Input() blob: string | Blob | Storage;
+  ready = false;
+}
+
+describe("StorageComponent", () => {
+  let fixture: ComponentFixture<StorageComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        MatProgressSpinnerModule,
+        MatProgressBarModule,
+        MatTooltipModule,
+        MatIconModule
+      ],
+      declarations: [StorageComponent, PickerDirective, StorageViewCmp],
+      providers: [
+        {
+          provide: INPUT_SCHEMA,
+          useValue: {
+            type: "storage",
+            $name: "test"
+          }
+        }
+      ]
+    });
+    fixture = TestBed.createComponent(StorageComponent);
+    fixture.detectChanges();
+  });
+
+  describe("basic behavior", () => {
+    it("should show name as title if title is undefined", () => {
+      fixture.componentInstance.schema.title = undefined;
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.css("section span h5")).nativeElement.textContent).toBe(
+        "test"
+      );
+    });
+
+    it("should show title", () => {
+      const title = (fixture.componentInstance.schema.title = "my title");
+      fixture.detectChanges();
+      expect(fixture.debugElement.query(By.css("section span h5")).nativeElement.textContent).toBe(
+        title
+      );
+    });
+
+    it("should show description if provided", () => {
+      expect(fixture.debugElement.query(By.css("section span small"))).toBeNull();
+      const description = (fixture.componentInstance.schema.description = "my long description");
+      fixture.detectChanges(false);
+      expect(
+        fixture.debugElement.query(By.css("section span small")).nativeElement.textContent
+      ).toBe(description);
+    });
+
+    it("should add class when disabled", () => {
+      fixture.componentInstance.setDisabledState(true);
+      fixture.detectChanges();
+      expect(fixture.debugElement.classes.disabled).toBe(true);
+    });
+
+    it("should write value and reset blob", () => {
+      fixture.componentInstance.blob = null;
+      fixture.componentInstance.writeValue("http://example/test.png");
+      expect(fixture.componentInstance.value).toBe("http://example/test.png");
+      expect(fixture.componentInstance.blob).toBeUndefined();
+    });
+
+    it("should not show clear button when empty", () => {
+      expect(
+        fixture.debugElement.query(By.css("section span:last-of-type button"))
+      ).not.toBeTruthy();
+    });
+
+    it("should clear", () => {
+      fixture.componentInstance.value = "http://example/test.png";
+      fixture.componentInstance.blob = null;
+      fixture.detectChanges();
+
+      fixture.debugElement.query(By.css("section span:last-of-type button")).nativeElement.click();
+      expect(fixture.componentInstance.value).toBeUndefined();
+      expect(fixture.componentInstance.blob).toBeUndefined();
+    });
+  });
+
+  describe("preview", () => {
+    it("should show drop are if empty", () => {
+      fixture.componentInstance.blob = fixture.componentInstance.value = undefined;
+      fixture.detectChanges();
+      expect(
+        fixture.debugElement.query(By.css("div.drop mat-icon")).nativeElement.textContent
+      ).toBe("save_alt");
+      expect(fixture.debugElement.query(By.css("div.drop small")).nativeElement.textContent).toBe(
+        "Drag & Drop your file here"
+      );
+    });
+
+    it("should show preview if value or blob is provided", () => {
+      fixture.componentInstance.value = "http://example/test.png";
+      fixture.detectChanges(false);
+      let preview = fixture.debugElement.query(By.directive(StorageViewCmp));
+
+      expect(fixture.debugElement.query(By.css("div.drop"))).not.toBeTruthy();
+      expect(preview.componentInstance.blob).toEqual("http://example/test.png");
+
+      fixture.componentInstance.blob = {
+        name: "",
+        size: 0,
+        type: "image/png"
+      };
+      fixture.detectChanges(false);
+      preview = fixture.debugElement.query(By.directive(StorageViewCmp));
+      expect(fixture.debugElement.query(By.css("mat-progress-bar"))).toBeTruthy();
+      expect(
+        fixture.debugElement.query(By.css("section:last-of-type mat-icon")).nativeElement
+          .textContent
+      ).toBe("cached");
+      expect(fixture.debugElement.query(By.css("div.drop"))).not.toBeTruthy();
+      expect(preview.componentInstance.blob).toEqual(fixture.componentInstance.blob);
+    });
+
+    it("should change preview icon to drop when dragging over", () => {
+      fixture.componentInstance.value = "http://example/test.png";
+      fixture.componentInstance.isDraggingOver = true;
+      fixture.detectChanges(false);
+      expect(
+        fixture.debugElement.query(By.css("section:last-of-type mat-icon")).nativeElement
+          .textContent
+      ).toBe("save_alt");
+    });
+
+    it("should hide progress bar when ready", () => {
+      fixture.componentInstance.value = "http://example/test.png";
+      fixture.detectChanges(false);
+
+      expect(fixture.debugElement.query(By.css("mat-progress-bar"))).toBeTruthy();
+
+      fixture.debugElement.query(By.directive(StorageViewCmp)).componentInstance.ready = true;
+      fixture.detectChanges();
+
+      expect(fixture.debugElement.query(By.css("mat-progress-bar"))).not.toBeTruthy();
+    });
+  });
+
+  describe("picker", () => {
+    it("should emit the changes from picker", () => {
+      const ngModelChange = jasmine.createSpy("ngModelChange");
+      fixture.componentInstance.registerOnChange(ngModelChange);
+
+      fixture.debugElement
+        .query(By.directive(PickerDirective))
+        .injector.get(PickerDirective)
+        .change.emit({
+          name: "",
+          url: "http://example/test.png",
+          size: 0,
+          type: "image/png"
+        });
+
+      expect(ngModelChange).toHaveBeenCalledTimes(1);
+      expect(ngModelChange).toHaveBeenCalledWith("http://example/test.png");
+    });
+  });
+
+  describe("upload", () => {
+    let httpTestingController: HttpTestingController;
+    const file = new File([], "test.png", {type: "image/png"});
+
+    beforeEach(() => {
+      httpTestingController = TestBed.get(HttpTestingController);
+    });
+
+    it("should handle drop", async () => {
+      const preventDefaultSpy = jasmine.createSpy("preventDefault");
+      fixture.debugElement.triggerEventHandler("drop", {
+        dataTransfer: {
+          files: {
+            0: file,
+            item: () => file,
+            length: 1
+          }
+        },
+        preventDefault: preventDefaultSpy
+      });
+      expect(preventDefaultSpy).toHaveBeenCalledTimes(1);
+
+      fixture.detectChanges();
+      await new Promise(resolve => setTimeout(resolve, 10));
+
+      httpTestingController.expectOne("api:/storage");
+    });
+
+    it("should report upload progress and remove spinner after complete", async () => {
+      fixture.debugElement.triggerEventHandler("drop", {
+        dataTransfer: {
+          files: {
+            0: file,
+            item: () => file,
+            length: 1
+          }
+        },
+        preventDefault: jasmine.createSpy()
+      });
+
+      fixture.detectChanges();
+
+      await new Promise(resolve => setTimeout(resolve, 10));
+
+      const req = httpTestingController.expectOne("api:/storage");
+
+      req.event({type: HttpEventType.UploadProgress, total: 100, loaded: 10});
+      fixture.detectChanges();
+
+      expect(
+        fixture.debugElement.query(By.directive(MatProgressSpinner)).componentInstance.value
+      ).toBe(10);
+
+      req.event(new HttpResponse({body: [{_id: 0}], url: "http://example"}));
+
+      expect(fixture.componentInstance.value).toBe("http://example/0");
+    });
+  });
+});
