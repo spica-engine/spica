8dc0a62ab4672efb3c4ed1efb20e09dfef098923
“htuna07” <h.tuna96@hotmail.com  git config --global user.name htuna07git config --global user.email h.tuna96@hotmail.com>
2019-12-23 11:13:24 +0300

    changed service with mock service

--- stacks/api/bucket/bucket.schema.resolver.spec.ts
+++ stacks/api/bucket/bucket.schema.resolver.spec.ts
@@ -1,69 +1,78 @@
 import {TestingModule, Test} from "@nestjs/testing";
 import {DatabaseTestingModule, DatabaseService, ObjectId} from "@spica-server/database/testing";
-import {PreferenceModule} from "@spica-server/preference";
+import {PreferenceModule, Preference} from "@spica-server/preference";
 import {SchemaModule} from "@spica-server/core/schema";
-import {BucketModule} from "./bucket.module";
 import {BucketSchemaResolver} from "./bucket.schema.resolver";
+import {BucketService} from "./bucket.service";
 
 describe("bucket service", () => {
   let module: TestingModule;
   let schemaResolver: BucketSchemaResolver;
-  const bucketId = new ObjectId();
+  let bs: BucketService;
+  const bucket = {
+    _id: new ObjectId(),
+    title: "New Bucket",
+    description: "Describe your new bucket",
+    icon: "view_stream",
+    primary: "title",
+    readOnly: false,
+    properties: {
+      title: {
+        type: "string",
+        title: "title",
+        description: "Title of the row",
+        options: {position: "left", visible: true}
+      },
+      description: {
+        type: "textarea",
+        title: "description",
+        description: "Description of the row",
+        options: {position: "right"}
+      },
+      text: {
+        type: "string",
+        title: "translatable text",
+        description: "Text of the row",
+        options: {position: "left", visible: true, translate: true}
+      }
+    }
+  };
+
+  const preference = {
+    language: {
+      available: {
+        tr_TR: "Turkish",
+        en_US: "English"
+      },
+      default: "en_US"
+    }
+  };
+
   beforeAll(async () => {
+    const mockBucketService = {
+      findOne: jasmine.createSpy("findOne").and.returnValue(Promise.resolve(bucket)),
+      getPreferences: jasmine
+        .createSpy("getPreferences")
+        .and.returnValue(Promise.resolve(preference))
+    };
+
     module = await Test.createTestingModule({
-      imports: [
-        DatabaseTestingModule.replicaSet(),
-        PreferenceModule,
-        SchemaModule.forChild(),
-        BucketModule
+      imports: [DatabaseTestingModule.create(), PreferenceModule, SchemaModule.forChild()],
+      providers: [
+        {
+          provide: BucketService,
+          useValue: mockBucketService
+        }
       ]
     }).compile();
-    schemaResolver = module.get(BucketSchemaResolver);
-
-    //add bucket
-    await module
-      .get(DatabaseService)
-      .collection("buckets")
-      .insertOne({
-        _id: bucketId,
-        title: "New Bucket",
-        description: "Describe your new bucket",
-        icon: "view_stream",
-        primary: "title",
-        readOnly: false,
-        properties: {
-          title: {
-            type: "string",
-            title: "title",
-            description: "Title of the row",
-            options: {position: "left", visible: true}
-          },
-          description: {
-            type: "textarea",
-            title: "description",
-            description: "Description of the row",
-            options: {position: "right"}
-          },
-          text: {
-            type: "string",
-            title: "translatable text",
-            description: "Text of the row",
-            options: {position: "left", visible: true, translate: true}
-          }
-        }
-      });
-  }, 30000);
-
-  afterAll(async () => {
-    await module
-      .get(DatabaseService)
-      .collection("buckets")
-      .deleteOne({_id: bucketId});
-    await module.close();
+    bs = module.get(BucketService);
+    schemaResolver = new BucketSchemaResolver(bs);
   });
 
+  afterAll(async () => {});
+
   it("should resolve bucket", async () => {
-    const response = await schemaResolver.resolve(bucketId.toHexString());
+    const response = await schemaResolver.resolve(bucket._id.toHexString());
     expect(response).toEqual({
       title: "New Bucket",
       description: "Describe your new bucket",
@@ -71,8 +80,8 @@ describe("bucket service", () => {
       primary: "title",
       readOnly: false,
       $schema: "http://spica.internal/bucket/schema",
-      _id: bucketId.toHexString(),
-      $id: bucketId.toHexString(),
+      _id: bucket._id,
+      $id: bucket._id,
       additionalProperties: false,
       properties: {
         _id: {type: "string", options: {position: undefined}, format: "objectid"},
