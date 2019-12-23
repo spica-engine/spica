4166c289601c4eddf58c4c42d1437524ec254855
tthiscafer <cafer.cafer@teknodev.biz>
2019-12-23 13:44:37 +0300

    fix test

--- stacks/spica/src/bucket/pages/index/index.component.spec.ts
+++ stacks/spica/src/bucket/pages/index/index.component.spec.ts
@@ -34,7 +34,7 @@ import {BucketDataService} from "../../services/bucket-data.service";
 import {BucketService} from "../../services/bucket.service";
 import {IndexComponent} from "./index.component";
 
-fdescribe("IndexComponent", () => {
+describe("IndexComponent", () => {
   let fixture: ComponentFixture<IndexComponent>;
   let bucket = new Subject<Partial<Bucket>>();
   let rows = new Subject<BucketRow[]>();
