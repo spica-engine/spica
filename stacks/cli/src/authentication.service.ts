0d8a3985409d4302671313b8c9723a302e613a68
“htuna07” <h.tuna96@hotmail.com  git config --global user.name htuna07git config --global user.email h.tuna96@hotmail.com>
2020-01-13 15:42:46 +0300

    identify function renamed as authenticate

--- stacks/cli/src/authentication.service.ts
+++ stacks/cli/src/authentication.service.ts
@@ -1,7 +1,7 @@
 import * as httpService from "./http.service";
 
 export function authenticate(username: string, password: string, serverUrl: string): Promise<any> {
-    return httpService.getRequest(
-      `${serverUrl}/passport/identify?password=${password}&identifier=${username}`
-    );
+  return httpService.getRequest(
+    `${serverUrl}/passport/identify?password=${password}&identifier=${username}`
+  );
 }
