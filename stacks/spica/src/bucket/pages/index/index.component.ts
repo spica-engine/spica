1032bc1732f9679ecb55f9af3f069f6278032205
tthiscafer <cafer.cafer@teknodev.biz>
2020-01-14 11:38:25 +0300

    fix(bucket): secondary property not found on bucket data index page

--- stacks/spica/src/bucket/pages/index/index.component.ts
+++ stacks/spica/src/bucket/pages/index/index.component.ts
@@ -115,19 +115,23 @@ export class IndexComponent implements OnInit {
 
         setTimeout(() => {
           const propertyNameFirst =
-            this.properties.length > 1 && this.properties[1].name
-              ? this.properties[1].name
-              : undefined;
+            this.properties.length > 0 ? this.properties[1].name : undefined;
+          const propertyNameSecond =
+            this.properties.length > 1 ? this.properties[2].name : undefined;
           const propertyDataFirst =
-            response.data.length > 0 ? response.data[0][this.properties[1].name] : "";
+            response.data.length > 0 && this.properties.length > 0
+              ? response.data[0][this.properties[1].name]
+              : "";
           const propertyDataSecond =
-            response.data.length > 0 ? response.data[0][this.properties[2].name] : "";
+            response.data.length > 0 && this.properties.length > 1
+              ? response.data[0][this.properties[2].name]
+              : "";
           this.guideUrls = {
             getAllWithLimit: `${bucketUrl}limit=3`,
             getAllWithSort: `${bucketUrl}limit=3&sort={"${propertyNameFirst}":1}`,
             getWithFilter: `${bucketUrl}limit=3&filter={"${propertyNameFirst}":{"$regex":"${propertyDataFirst}"}}`,
             getWithLike: `${bucketUrl}limit=3&filter={"${propertyNameFirst}":{"$regex":"${propertyDataFirst}"}}`,
-            getWithDoubleFilter: `${bucketUrl}limit=1&filter={"${propertyNameFirst}":{"$regex":"${propertyDataFirst}"},"${this.properties[2].name}":{"$regex":"${propertyDataSecond}"}}`,
+            getWithDoubleFilter: `${bucketUrl}limit=1&filter={"${propertyNameFirst}":{"$regex":"${propertyDataFirst}"},"${propertyNameSecond}":{"$regex":"${propertyDataSecond}"}}`,
             getOnlyScheduled: `${bucketUrl}paginate=true&limit=3&schedule=true`,
             getDataWithLang: `${bucketUrl}limit=3`
           };
