9dd855840cfe3f6316daf9faa9d0753caee563d8
batu07040 <borsayusuf07@gmail.com>
2020-01-21 12:04:51 +0300

    added support policy page.

---
+++ docs/site/src/app/pages/support-policy/support-policy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-support-policy',
+  templateUrl: './support-policy.component.html',
+  styleUrls: ['./support-policy.component.scss']
+})
+export class SupportPolicyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
