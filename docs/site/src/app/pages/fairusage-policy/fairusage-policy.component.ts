7bc08b7a86e1906ff7b308c43834648d5be16f59
batu07040 <borsayusuf07@gmail.com>
2020-01-21 11:59:42 +0300

    added fairusage policy page

---
+++ docs/site/src/app/pages/fairusage-policy/fairusage-policy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fairusage-policy',
+  templateUrl: './fairusage-policy.component.html',
+  styleUrls: ['./fairusage-policy.component.scss']
+})
+export class FairusagePolicyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
