d3f52e7869c60bc3d9dd0efe663a0908c948a7d2
batu07040 <borsayusuf07@gmail.com>
2020-01-21 11:59:10 +0300

    added limit policy page

---
+++ docs/site/src/app/pages/limit-policy/limit-policy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-limit-policy',
+  templateUrl: './limit-policy.component.html',
+  styleUrls: ['./limit-policy.component.scss']
+})
+export class LimitPolicyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
