86c7f5551c7cb51cf163c2dba6ea2a5a1c1680c8
batu07040 <borsayusuf07@gmail.com>
2020-01-21 12:02:36 +0300

    demo data added partners for partner page

--- docs/site/src/app/pages/partners/partners.component.ts
+++ docs/site/src/app/pages/partners/partners.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from "@angular/core";
 import {fly, flyOne} from "../animations";
+import {countryList, partnerList} from "../../services/partners.service";
+
 @Component({
   selector: "app-partners",
   templateUrl: "./partners.component.html",
@@ -7,7 +9,21 @@ import {fly, flyOne} from "../animations";
   animations: [fly("fly"), flyOne("flyOne")]
 })
 export class PartnersComponent implements OnInit {
+  countryList = countryList;
+  partnerList = partnerList;
+  partnerFilter = [];
+  selectedCountry;
   constructor() {}
 
   ngOnInit() {}
+
+  countrySelect() {
+    this.partnerFilter = [];
+    partnerList.find(partner => {
+      if ((partner.country == this.selectedCountry) == true) {
+        this.partnerFilter.push(partner);
+      }
+      return null;
+    });
+  }
 }
