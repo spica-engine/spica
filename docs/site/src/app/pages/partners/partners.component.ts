1dba883d06dc3bb82ee83b03e4bb86867273dc3e
batu07040 <borsayusuf07@gmail.com>
2020-02-05 16:17:27 +0300

    fixed the variables and data getting.

--- docs/site/src/app/pages/partners/partners.component.ts
+++ docs/site/src/app/pages/partners/partners.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from "@angular/core";
-import {fly, flyOne} from "../animations";
-import {countryList, partnerList} from "../../services/partners.service";
+import {fly, flyOne} from "../../pages/animations";
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import countries from "../../../assets/countries.json";
+import partners from "../../../assets/partners.json";
 
 @Component({
   selector: "app-partners",
@@ -9,17 +12,32 @@ import {countryList, partnerList} from "../../services/partners.service";
   animations: [fly("fly"), flyOne("flyOne")]
 })
 export class PartnersComponent implements OnInit {
-  countryList = countryList;
-  partnerList = partnerList;
-  partnerFilter = [];
-  selectedCountry;
-  constructor() {}
+  countryList: object;
+  partnerList: object;
+  partnerFilter: any = [];
+  selectedCountry: string;
+  constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getCountries().subscribe(r => {
+      this.countryList = r;
+    });
+
+    this.getPartners().subscribe(r => {
+      this.partnerList = r;
+    });
+  }
+
+  getCountries(): Observable<any> {
+    return this.http.get("../../../assets/countries.json");
+  }
+  getPartners(): Observable<any> {
+    return this.http.get("../../../assets/partners.json");
+  }
 
   countrySelect() {
     this.partnerFilter = [];
-    partnerList.find(partner => {
+    Object(this.partnerList).find(partner => {
       if ((partner.country == this.selectedCountry) == true) {
         this.partnerFilter.push(partner);
       }
