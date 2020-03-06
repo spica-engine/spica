8912c6b9d90d32121ff2876138e443c87adb6621
“htuna07” <h.tuna96@hotmail.com  git config --global user.name htuna07git config --global user.email h.tuna96@hotmail.com>
2020-03-06 14:37:01 +0300

    removed unused imports

--- stacks/spica/src/function/function.module.ts
+++ stacks/spica/src/function/function.module.ts
@@ -1,5 +1,5 @@
 import {CommonModule} from "@angular/common";
-import {ModuleWithProviders, NgModule, Injectable} from "@angular/core";
+import {ModuleWithProviders, NgModule} from "@angular/core";
 import {FormsModule} from "@angular/forms";
 import {MatButtonModule} from "@angular/material/button";
 import {MatCardModule} from "@angular/material/card";
