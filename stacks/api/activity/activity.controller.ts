f2a84ec224c386ccf4eac7887888ffb3c7d58731
“htuna07” <h.tuna96@hotmail.com  git config --global user.name htuna07git config --global user.email h.tuna96@hotmail.com>
2020-03-22 21:36:46 +0300

    controller get endpoint completed

--- stacks/api/activity/activity.controller.ts
+++ stacks/api/activity/activity.controller.ts
@@ -1,11 +1,51 @@
-import {Controller, Get} from "@nestjs/common";
+import {Controller, Get, Query} from "@nestjs/common";
 import {ActivityService} from "./activity.service";
+import {Activity, Resource} from ".";
+import {JSONP, DATE} from "@spica-server/core";
+import {ObjectID} from "mongodb";
 
 @Controller("activity")
 export class ActivityController {
   constructor(private activityService: ActivityService) {}
   @Get()
-  findAll() {
-    return this.activityService.find();
+  find(
+    @Query("identifier") identifier = "",
+    @Query("action") action: string,
+    @Query("resource", JSONP) resource: Resource,
+    @Query("begin", DATE) begin: Date,
+    @Query("end", DATE) end: Date
+  ): Promise<Activity[]> {
+    let filter = {
+      identifier: {
+        $regex: `^${identifier}`
+      }
+    };
+
+    if (!isNaN(begin.getTime()) && !isNaN(end.getTime())) {
+      filter["_id"] = {
+        $gte: ObjectID.createFromTime(begin.getTime() / 1000),
+        $lt: ObjectID.createFromTime(end.getTime() / 1000)
+      };
+    }
+
+    if (action) {
+      filter["action"] = action;
+    }
+
+    if (resource) {
+      if (resource.moduleName) {
+        filter["resource.moduleName"] = resource.moduleName;
+      }
+      if (resource.moduleId) {
+        filter["resource.moduleId"] = resource.moduleId;
+      }
+      if (resource.documentId) {
+        filter["resource.documentId"] = resource.documentId;
+      }
+    }
+
+    console.log(filter)
+
+    return this.activityService.find(filter);
   }
 }
