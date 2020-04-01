ac30486f0ad96da2e9c2039fe227cba9f0ab1d41
“htuna07” <h.tuna96@hotmail.com  git config --global user.name htuna07git config --global user.email h.tuna96@hotmail.com>
2020-04-01 15:26:13 +0300

    test created

--- stacks/spica/src/activity/pages/index/index.component.spec.ts
+++ stacks/spica/src/activity/pages/index/index.component.spec.ts
@@ -1,20 +1,77 @@
-import {async, ComponentFixture, TestBed} from "@angular/core/testing";
+import {IndexComponent} from "@spica-client/activity/pages/index/index.component";
+import {
+  MatIconModule,
+  MatToolbarModule,
+  MatCardModule,
+  MatTableModule,
+  MatOptionModule,
+  MatSelectModule,
+  MatInputModule,
+  MatButtonModule
+} from "@angular/material";
+import {ScrollingModule} from "@angular/cdk/scrolling";
+import {MatListModule} from "@angular/material/list";
+import {NoopAnimationsModule} from "@angular/platform-browser/animations";
+import {SatDatepickerModule, SatNativeDateModule} from "saturn-datepicker";
+import {FormsModule} from "@angular/forms";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {ActivityService} from "../../services/activity.service";
 
-import {IndexComponent} from "./index.component";
+import {async, ComponentFixture, TestBed} from "@angular/core/testing";
+import {of} from "rxjs";
 
 describe("IndexComponent", () => {
   let component: IndexComponent;
   let fixture: ComponentFixture<IndexComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(async () => {
     TestBed.configureTestingModule({
-      declarations: [IndexComponent]
+      imports: [
+        NoopAnimationsModule,
+        MatIconModule,
+        MatToolbarModule,
+        MatCardModule,
+        MatTableModule,
+        MatOptionModule,
+        MatSelectModule,
+        MatInputModule,
+        SatDatepickerModule,
+        SatNativeDateModule,
+        FormsModule,
+        MatButtonModule,
+        ScrollingModule,
+        MatListModule,
+        MatProgressSpinnerModule
+      ],
+      declarations: [IndexComponent],
+      providers: [
+        {
+          provide: ActivityService,
+          useValue: {
+            get: () => {
+              return of([
+                {
+                  action: "test_action",
+                  identifier: "test_identifier",
+                  resource: {documentId: ["test_document"], name: "test_name"},
+                  date: new Date()
+                }
+              ]);
+            },
+            getDocuments: () => {
+              return of(["doc_1", "doc_2"]);
+            }
+          }
+        }
+      ]
     }).compileComponents();
-  }));
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(IndexComponent);
     component = fixture.componentInstance;
+
+    fixture.detectChanges();
+
+    await fixture.whenStable();
     fixture.detectChanges();
   });
 
