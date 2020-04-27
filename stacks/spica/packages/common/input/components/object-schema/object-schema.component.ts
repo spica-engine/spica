f3440ad785cf795edd92004c428ac1b66bb3b716
htuna07 <h.tuna96@gmail.com>
2020-04-27 11:06:49 +0300

    fix(bucket): bucket tries to validate deleted field in an object if its required  (#276)

--- stacks/spica/packages/common/input/components/object-schema/object-schema.component.ts
+++ stacks/spica/packages/common/input/components/object-schema/object-schema.component.ts
@@ -30,8 +30,8 @@ export class ObjectSchemaComponent {
 
   removeProperty(name: string) {
     delete this.schema.properties[name];
-    this.schema.required &&
-      this.schema.required.includes(name) &&
-      delete this.schema.required[name];
+    if (this.schema.required && this.schema.required.includes(name)) {
+      this.schema.required = this.schema.required.filter(req => req != name);
+    }
   }
 }
