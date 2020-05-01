272dbfe0cb005f12a5c2480660ef3b2d7c95d2b4
thesayyn <thesayyn@gmail.com>
2020-05-01 16:43:04 +0300

    fix: bootstrap with ssl

--- stacks/api/main.ts
+++ stacks/api/main.ts
@@ -1,26 +1,23 @@
 import {NestFactory} from "@nestjs/core";
-import {ExpressAdapter} from "@nestjs/platform-express";
 import {Middlewares} from "@spica-server/core";
-import * as express from "express";
 import * as fs from "fs";
-import * as http from "http";
 import * as https from "https";
 import {AppModule} from "./app.module";
 
 async function bootstrap() {
-  const server = express();
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-  app.use(Middlewares.BsonBodyParser, Middlewares.MergePatchJsonParser, Middlewares.Preflight);
-  await app.init();
-  http.createServer(server).listen(process.env.PORT);
+  let httpsOptions: https.ServerOptions;
 
-  if (process.env.HTTPS_PORT) {
-    const httpsOptions: https.ServerOptions = {
+  if (process.env.TLS_KEY_PATH && process.env.TLS_CERT_PATH) {
+    httpsOptions = {
       key: fs.readFileSync(process.env.TLS_KEY_PATH),
       cert: fs.readFileSync(process.env.TLS_CERT_PATH)
     };
-
-    https.createServer(httpsOptions, server).listen(process.env.HTTPS_PORT);
   }
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions
+  });
+  app.use(Middlewares.BsonBodyParser, Middlewares.MergePatchJsonParser, Middlewares.Preflight);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
