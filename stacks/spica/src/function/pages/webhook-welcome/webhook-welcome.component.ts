c91d1f72b6527d5b94671c301e6abbbbc81b3b22
“htuna07” <h.tuna96@hotmail.com  git config --global user.name htuna07git config --global user.email h.tuna96@hotmail.com>
2020-05-06 13:41:56 +0300

    lint

--- stacks/spica/src/function/pages/webhook-welcome/webhook-welcome.component.ts
+++ stacks/spica/src/function/pages/webhook-welcome/webhook-welcome.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
 
 @Component({
-  selector: 'app-webhook-welcome',
-  templateUrl: './webhook-welcome.component.html',
-  styleUrls: ['./webhook-welcome.component.scss']
+  selector: "app-webhook-welcome",
+  templateUrl: "./webhook-welcome.component.html",
+  styleUrls: ["./webhook-welcome.component.scss"]
 })
 export class WebhookWelcomeComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
