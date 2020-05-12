ef02d416bc7d5e2f0bf6c69d7ccb48c45786cd12
Sahin Yort <thesayyn@gmail.com>
2020-05-12 11:44:17 +0300

    feat(function): ability to filter channels (#303)

--- stacks/api/function/runtime/io/database.ts
+++ stacks/api/function/runtime/io/database.ts
@@ -1,29 +1,35 @@
 import {Collection, DatabaseService} from "@spica-server/database";
 import {PassThrough, Transform, Writable} from "stream";
-import {StdOut, StdOutOptions} from "./stdout";
+import {StandartStream, StreamOptions} from "./standart_stream";
 
-export class DatabaseOutput extends StdOut {
+export class DatabaseOutput extends StandartStream {
   private collection: Collection;
   constructor(private db: DatabaseService) {
     super();
     this.collection = this.db.collection("function_logs");
   }
 
-  create(options: StdOutOptions): Writable {
-    const transform = new Transform({
-      transform: (data, _, callback) => {
-        this.collection
-          .insertOne({
-            function: options.functionId,
-            event_id: options.eventId,
-            content: Buffer.from(data)
-              .toString()
-              .trim()
-          })
-          .then(() => callback(undefined, data))
-          .catch(error => callback(error));
-      }
-    });
-    return new PassThrough().pipe(transform);
+  create(options: StreamOptions): [Writable, Writable] {
+    const createTransform = (channel: "stderr" | "stdout") => {
+      return new Transform({
+        transform: (data, _, callback) => {
+          this.collection
+            .insertOne({
+              function: options.functionId,
+              event_id: options.eventId,
+              channel,
+              content: Buffer.from(data)
+                .toString()
+                .trim()
+            })
+            .then(() => callback(undefined, data))
+            .catch(error => callback(error));
+        }
+      });
+    };
+    return [
+      new PassThrough().pipe(createTransform("stdout")),
+      new PassThrough().pipe(createTransform("stderr"))
+    ];
   }
 }
