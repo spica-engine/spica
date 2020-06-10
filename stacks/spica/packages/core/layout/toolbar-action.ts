bc369266faef63d6968ab59d635205fd76033bbe
thesayyn <thesayyn@gmail.com>
2020-06-10 17:53:41 +0300

    fix: various fixes for function and passport module

---
+++ stacks/spica/packages/core/layout/toolbar-action.ts
@@ -0,0 +1,6 @@
+import {Directive} from "@angular/core";
+
+@Directive({
+  selector: "toolbar-action"
+})
+export class ToolbarActionDirective {}
--- stacks/spica/src/passport/directives/identity-badge/identity-badge.component.ts
+++ stacks/spica/src/passport/directives/identity-badge/identity-badge.component.ts
@@ -1,18 +1,30 @@
 import {Component} from "@angular/core";
 import {Router} from "@angular/router";
+import {Scheme, SchemeObserver} from "@spica-client/core/layout";
+import {Observable} from "rxjs";
+import {startWith} from "rxjs/operators";
 import {Identity} from "../../interfaces/identity";
 import {PassportService} from "../../services/passport.service";
 
 @Component({
-  selector: "passport-identity-badge",
+  selector: "passport-identity-action",
   templateUrl: "./identity-badge.component.html",
   styleUrls: ["./identity-badge.component.scss"]
 })
 export class IdentityBadgeComponent {
   identity: Identity;
 
-  constructor(private passportService: PassportService, private router: Router) {
+  dark$: Observable<boolean>;
+
+  constructor(
+    private passportService: PassportService,
+    private router: Router,
+    private schemeObserver: SchemeObserver
+  ) {
     this.identity = this.passportService.decodedToken;
+    this.dark$ = this.schemeObserver
+      .observe(Scheme.Dark)
+      .pipe(startWith(this.schemeObserver.isMatched(Scheme.Dark)));
   }
 
   unidentify() {
