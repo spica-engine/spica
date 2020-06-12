50fffdbd1db2b39600406a7461e602d1c26bfef8
“htuna07” <h.tuna96@hotmail.com  git config --global user.name htuna07git config --global user.email h.tuna96@hotmail.com>
2020-06-12 17:41:09 +0300

    test fix

--- stacks/api/bucket/hooks/test/src/integration.spec.ts
+++ stacks/api/bucket/hooks/test/src/integration.spec.ts
@@ -184,8 +184,8 @@ describe("Hooks Integration", () => {
   });
 
   afterEach(() => {
-    req.delete(`/bucket/${bucket._id}/data/${user1._id}`, headers).catch(() => {});
-    req.delete(`/bucket/${bucket._id}/data/${user2._id}`, headers).catch(() => {});
+    req.delete(`/bucket/${bucket._id}/data/${user1._id}`, {}, headers).catch(() => {});
+    req.delete(`/bucket/${bucket._id}/data/${user2._id}`, {}, headers).catch(() => {});
   });
 
   describe("GET", () => {
