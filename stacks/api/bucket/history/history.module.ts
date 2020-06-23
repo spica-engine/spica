81c621995a9a88c80da0a5d8637c234dc512919b
htuna07 <h.tuna96@gmail.com>
2020-06-23 18:12:10 +0300

    refactor: move generation of the histories to rest apis (#361)

--- stacks/api/bucket/history/history.module.ts
+++ stacks/api/bucket/history/history.module.ts
@@ -1,14 +1,10 @@
 import {Module} from "@nestjs/common";
-import {BucketWatcher} from "./watcher";
 import {HistoryController} from "./history.controller";
 import {HistoryService} from "./history.service";
 
 @Module({
   controllers: [HistoryController],
-  providers: [BucketWatcher, HistoryService]
+  providers: [HistoryService],
+  exports: [HistoryService]
 })
-export class HistoryModule {
-  constructor(bw: BucketWatcher) {
-    bw.watch();
-  }
-}
+export class HistoryModule {}
