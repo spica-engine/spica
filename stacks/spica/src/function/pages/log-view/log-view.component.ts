d799cfee79434829ffb9b38c2e45bccf4624f6d1
htuna07 <h.tuna96@gmail.com>
2020-07-21 12:52:15 +0300

    remove unused imports

--- stacks/spica/src/function/pages/log-view/log-view.component.ts
+++ stacks/spica/src/function/pages/log-view/log-view.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, OnDestroy, ViewChild, TemplateRef} from "@angular/core";
+import {Component, OnInit, OnDestroy} from "@angular/core";
 import {ActivatedRoute, Router} from "@angular/router";
 import {Observable, forkJoin, Subject} from "rxjs";
 import {switchMap, tap, map, filter, take, flatMap, takeUntil} from "rxjs/operators";
