1087d2cf08cad92b84475c954601896e36fe6844
thesayyn <thesayyn@gmail.com>
2020-08-04 16:36:40 +0300

    fix(cli): fix port issue and provide an integration test

---
+++ stacks/cli/test/e2e/serve.spec.sh
@@ -0,0 +1,33 @@
+set +e
+
+source $1 $@
+
+echo "## Serving a spica instance"
+
+NAME="${TAG//_/-}"
+
+OUTPUT="$(spica serve $NAME --force --version=$TAG --image-pull-policy=if-not-present --no-open 2>&1)"
+
+assert_partially "$OUTPUT" "Pulling images" 
+assert_partially "$OUTPUT" "Creating an ingress to route traffic."
+assert_partially "$OUTPUT" "Spica $NAME is serving on http://localhost"
+
+
+PORT_REGEX='on http:\/\/localhost\:([0-9]+)'
+
+if [[ "$OUTPUT" =~ $PORT_REGEX ]] 
+    then 
+        PORT=${BASH_REMATCH[1]}
+    else 
+        echo "Could not find the port."
+        exit 1
+    fi
+
+
+OUTPUT="$(docker run --network host --rm curlimages/curl curl -w '%{http_code} %{content_type}'  http://localhost:$PORT/api/bucket)"
+
+assert_partially "$OUTPUT" "401"
+assert_partially "$OUTPUT" "application/json"
+
+echo "## All tests have passed. Cleaning"
+spica rm $NAME
