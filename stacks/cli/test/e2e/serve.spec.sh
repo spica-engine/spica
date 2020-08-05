569377f6d4e7f4c6c2e047f13d5e0ec7a60aa05d
thesayyn <thesayyn@gmail.com>
2020-08-05 14:06:49 +0300

    fix ports

--- stacks/cli/test/e2e/serve.spec.sh
+++ stacks/cli/test/e2e/serve.spec.sh
@@ -7,7 +7,7 @@ NAME="${TAG//_/-}"
 # FD 5
 exec 5>&1
 
-OUTPUT=$(spica serve $NAME --force --version=$TAG --image-pull-policy=if-not-present --retain-volumes=false --no-open 2>&1 | tee /dev/fd/5; exit ${PIPESTATUS[0]})
+OUTPUT=$(spica serve $NAME --force --version=$TAG --port=5791 --image-pull-policy=if-not-present --retain-volumes=false --no-open 2>&1 | tee /dev/fd/5; exit ${PIPESTATUS[0]})
 
 assert_contains "$OUTPUT" "Pulling images" 
 assert_contains "$OUTPUT" "Creating an ingress to route traffic."
