5373616f486b87413497274daa965a3a8e7fc2e5
htuna07 <h.tuna96@gmail.com>
2020-12-09 18:25:27 +0300

    client updates

--- stacks/spica/src/dashboard/pages/dashboard-view/dashboard-view.component.ts
+++ stacks/spica/src/dashboard/pages/dashboard-view/dashboard-view.component.ts
@@ -1,6 +1,6 @@
 import {Component} from "@angular/core";
 import {ActivatedRoute} from "@angular/router";
-import {Observable} from "rxjs";
+import {Observable, BehaviorSubject} from "rxjs";
 import {switchMap, tap} from "rxjs/operators";
 import {DashboardService} from "../../services/dashboard.service";
 import {Dashboard} from "@spica-client/dashboard/interfaces";
@@ -13,11 +13,39 @@ import {Dashboard} from "@spica-client/dashboard/interfaces";
 export class DashboardViewComponent {
   dashboard$: Observable<Dashboard>;
 
+  componentData$: Observable<object>[] = [];
+
+  refreshSubjects$: BehaviorSubject<any>[] = [];
+
+  defaultTypes = ["line", "pie", "doughnut", "polarArea", "scatter", "bubble", "radar", "bar"];
+
+  customTypes = ["table"];
+
   constructor(private activatedRoute: ActivatedRoute, private ds: DashboardService) {}
 
   ngOnInit() {
     this.dashboard$ = this.activatedRoute.params.pipe(
-      switchMap(params => this.ds.getDashboard(params.id))
+      switchMap(params =>
+        this.ds.getDashboard(params.id).pipe(
+          tap(dashboard => {
+            if (!dashboard.components) {
+              return;
+            }
+            
+            for (const component of dashboard.components) {
+              const refresh$ = new BehaviorSubject(undefined);
+              this.refreshSubjects$.push(refresh$);
+              this.componentData$.push(
+                refresh$.pipe(switchMap(() => this.ds.executeComponent(component.url)))
+              );
+            }
+          })
+        )
+      )
     );
+  }
+
+  refresh(i: number) {
+    this.refreshSubjects$[i].next(undefined);
   }
 }
--- stacks/spica/src/dashboard/services/dashboard.service.ts
+++ stacks/spica/src/dashboard/services/dashboard.service.ts
@@ -1,7 +1,7 @@
-import {HttpClient, HttpParams} from "@angular/common/http";
+import {HttpClient} from "@angular/common/http";
 import {Injectable} from "@angular/core";
 import {Store} from "@ngrx/store";
-import {Observable, of} from "rxjs";
+import {Observable} from "rxjs";
 import {tap} from "rxjs/operators";
 import {Dashboard} from "../interfaces";
 import * as fromDashboard from "../state/dashboard.reducer";
@@ -19,15 +19,7 @@ export class DashboardService {
   }
 
   executeComponent(url: string) {
-    return of({
-      title: "line title",
-      options: {legend: {display: true}, responsive: true},
-      label: ["1", "2", "3", "4", "5", "6"],
-      datasets: [{data: [65, 59, 90, 81, 56, 55, 40], label: "linedata"}],
-      legend: true,
-      width: 70,
-      filters: [{key: "line_data_filter", title: "Please enter filter", type: "string"}]
-    });
+    return this.http.get(url);
   }
 
   retrieve() {
