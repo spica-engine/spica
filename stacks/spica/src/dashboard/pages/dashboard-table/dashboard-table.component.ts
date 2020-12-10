bbee76c37099a2cf75dbad10655f1f56c41817c3
htuna07 <h.tuna96@gmail.com>
2020-12-10 14:15:30 +0300

    tests updated

--- stacks/spica/src/dashboard/pages/dashboard-table/dashboard-table.component.ts
+++ stacks/spica/src/dashboard/pages/dashboard-table/dashboard-table.component.ts
@@ -1,9 +1,4 @@
-import {
-  Component,
-  Input,
-  OnInit,
-  ViewChild,
-} from "@angular/core";
+import {Component, Input, OnInit, ViewChild} from "@angular/core";
 import {MatSort} from "@angular/material/sort";
 import {MatTableDataSource} from "@angular/material/table";
 import {MatPaginator} from "@angular/material/paginator";
