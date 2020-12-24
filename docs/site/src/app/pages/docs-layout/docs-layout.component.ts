190f02a3428259aead339e69d5d644882aa256b8
Akhnos <teknodev1@Teknodev1s-MacBook-Pro.local>
2020-12-24 16:17:44 +0300

    documentation change

--- docs/site/src/app/pages/docs-layout/docs-layout.component.ts
+++ docs/site/src/app/pages/docs-layout/docs-layout.component.ts
@@ -1,7 +1,8 @@
 import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
-import {Component, OnInit} from "@angular/core";
+import {Component, ElementRef, OnInit, ViewChild} from "@angular/core";
+import {NavigationEnd, Router} from "@angular/router";
 import {Observable} from "rxjs";
-import {map, startWith} from "rxjs/operators";
+import {filter, map, startWith, tap} from "rxjs/operators";
 import {DocService} from "../../services/doc.service";
 
 @Component({
@@ -12,6 +13,8 @@ import {DocService} from "../../services/doc.service";
 export class DocsLayoutComponent implements OnInit {
   $apiDocs: Observable<any>;
   $contentDocs: Observable<any>;
+  headingsInContent: {title: string; fragment: string}[];
+  activeDocument: string;
   isHandset$: Observable<boolean> = this.breakpointObserver
     .observe([Breakpoints.Handset, Breakpoints.Tablet])
     .pipe(
@@ -19,9 +22,25 @@ export class DocsLayoutComponent implements OnInit {
       startWith(true)
     );
 
-  constructor(docs: DocService, private breakpointObserver: BreakpointObserver) {
+  constructor(
+    docs: DocService,
+    private breakpointObserver: BreakpointObserver,
+    private route: Router
+  ) {
     this.$apiDocs = docs.getApiDocs();
     this.$contentDocs = docs.getContentDocs();
+
+    docs.documentChanged.subscribe(data => {
+      this.headingsInContent = [];
+      for (let [i, heading] of data.entries()) {
+        this.headingsInContent[i] = {
+          title: heading,
+          fragment: heading.toLowerCase().replace(/ /g, "-")
+        };
+      }
+      let url = this.route.url;
+      this.activeDocument = url.split("/")[url.split("/").length - 1];
+    });
   }
 
   ngOnInit() {}
