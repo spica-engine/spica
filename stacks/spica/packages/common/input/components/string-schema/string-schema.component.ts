d1ce4a4e70e615aa80234c0f082fa125830e7b47
htuna07 <h.tuna96@gmail.com>
2020-12-31 17:57:07 +0300

    fix(bucket): update default and min max values on bucket schema

--- stacks/spica/packages/common/input/components/string-schema/string-schema.component.ts
+++ stacks/spica/packages/common/input/components/string-schema/string-schema.component.ts
@@ -1,13 +1,15 @@
 import {Component, Inject} from "@angular/core";
-
 import {INPUT_SCHEMA, InputSchema} from "../../input";
+import {SchemaComponent} from "../schema.component";
 
 @Component({
   templateUrl: "./string-schema.component.html",
   styleUrls: ["./string-schema.component.scss"]
 })
-export class StringSchemaComponent {
-  constructor(@Inject(INPUT_SCHEMA) public schema: InputSchema) {}
+export class StringSchemaComponent extends SchemaComponent {
+  constructor(@Inject(INPUT_SCHEMA) public schema: InputSchema) {
+    super(schema);
+  }
   removeEnum() {
     delete this.schema.enum;
   }
