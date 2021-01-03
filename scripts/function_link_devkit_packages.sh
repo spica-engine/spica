3aac96d2ed9fadd26969713e8aa332b1332c9479
htuna07 <h.tuna96@gmail.com>
2021-01-03 18:29:43 +0300

    0.8.0-fix

--- scripts/function_link_devkit_packages.sh
+++ scripts/function_link_devkit_packages.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 set -u -e -o pipefail
 
-echo "Building the database, dashboard and bucket package"
-yarn bazel build //stacks/api/function/packages/database:package //stacks/api/function/packages/dashboard:package //stacks/api/function/packages/bucket:package
+echo "Building the database and bucket packages"
+yarn bazel build //stacks/api/function/packages/database:package //stacks/api/function/packages/bucket:package
 
-(cd "./bazel-bin/stacks/api/function/packages/dashboard/package")
 (cd "./bazel-bin/stacks/api/function/packages/database/package")
 (cd "./bazel-bin/stacks/api/function/packages/bucket/package")
 
@@ -17,12 +16,9 @@ FUNCTION_PATH=${FUNCTION_PATH:=/private/tmp/functions}
 
 for DIR in $FUNCTION_PATH/*
 do
-  DASHBOARD_PATH="$DIR/node_modules/@spica-devkit/dashboard"
   DATABASE_PATH="$DIR/node_modules/@spica-devkit/database"
   BUCKET_PATH="$DIR/node_modules/@spica-devkit/bucket"
 
-  (mkdir -p $DASHBOARD_PATH && rsync -ar --no-owner --no-group ./bazel-bin/stacks/api/function/packages/dashboard/package/* $DASHBOARD_PATH)
-  (cd $DASHBOARD_PATH && npm install)
   (mkdir -p $DATABASE_PATH && rsync -ar --no-owner --no-group ./bazel-bin/stacks/api/function/packages/database/package/* $DATABASE_PATH)
   (cd $DATABASE_PATH && npm install)
   (mkdir -p $BUCKET_PATH && rsync -ar --no-owner --no-group ./bazel-bin/stacks/api/function/packages/bucket/package/* $BUCKET_PATH)
