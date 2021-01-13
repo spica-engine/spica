50659fdfa7c01c6e571d05f7d03ebb0f6f41feaa
htuna07 <h.tuna96@gmail.com>
2021-01-13 12:15:44 +0300

    fix(passport): remove policy from apikey and identity after it removed

--- stacks/api/passport/policy/src/interface.ts
+++ stacks/api/passport/policy/src/interface.ts
@@ -16,3 +16,8 @@ export interface Statement {
       };
   module: string;
 }
+
+export const APIKEY_POLICY_FINALIZER = Symbol.for("APIKEY_POLICY_FINALIZER");
+export const IDENTITY_POLICY_FINALIZER = Symbol.for("IDENTITY_POLICY_FINALIZER");
+
+export type changeFactory = (policyId: string) => Promise<unknown>;
