66c1bd672f290b116131239bd6e8f4c0713e4048
htuna07 <h.tuna96@gmail.com>
2021-01-19 10:30:49 +0300

    initial commit

--- stacks/spica/src/passport/directives/can-interact.directive.ts
+++ stacks/spica/src/passport/directives/can-interact.directive.ts
@@ -1,23 +1,53 @@
-import {Directive, HostBinding, Input, OnInit, SimpleChanges} from "@angular/core";
+import {
+  Directive,
+  HostBinding,
+  Input,
+  OnInit,
+  SimpleChanges,
+  ElementRef,
+  Renderer2,
+  HostListener,
+  Output,
+  EventEmitter
+} from "@angular/core";
 import {PassportService} from "../services/passport.service";
 
 @Directive({selector: "[canInteract]"})
 export class CanInteractDirectiveTest {
-  @HostBinding("style.visibility") _visible = "visible";
   @Input("canInteract") action: string;
   @Input("resource") resource: string;
 }
 
 @Directive({selector: "[canInteract]"})
 export class CanInteractDirective implements OnInit {
-  @HostBinding("style.visibility") _visible = "hidden";
+  @HostBinding("disabled") isDisabled: boolean | undefined;
   @Input("canInteract") action: string;
   @Input() resource: string;
 
-  constructor(private passport: PassportService) {}
+  @Output() click: EventEmitter<unknown> = new EventEmitter();
+
+  tooltipTitle: string = "";
+  placement: string = "bottom";
+  delay: number = 200;
+  tooltip: HTMLElement;
+  offset = 10;
+
+  allowed = true;
+
+  isInitialized = false;
+
+  constructor(
+    private passport: PassportService,
+    private elementRef: ElementRef<HTMLElement>,
+    private renderer: Renderer2
+  ) {}
 
   ngOnInit(): void {
     this.setVisible(this.action, this.resource);
+  }
+
+  ngOnDestroy() {
+    this.mouseLeave();
   }
 
   ngOnChanges(changes: SimpleChanges) {
@@ -38,7 +68,114 @@ export class CanInteractDirective implements OnInit {
       .checkAllowed(action, resource)
       .toPromise()
       .then(allowed => {
-        this._visible = allowed ? "visible" : "hidden";
+        this.allowed = allowed;
+        this.tooltipTitle = `${this.action} is missing.`;
+
+        if (!this.allowed) {
+          this.renderer.addClass(this.elementRef.nativeElement, "ng-disabled-button");
+        }
       });
+  }
+
+  mouseEnter = (hostPos: DOMRect) => {
+    return () => {
+      // clear tooltips which may added before
+      if (this.tooltip) {
+        this.renderer.removeClass(this.tooltip, "ng-tooltip-show");
+        this.renderer.removeChild(document.body, this.tooltip);
+        this.tooltip = null;
+      }
+
+      this.create();
+
+      // SET POSITION
+      this.setPosition(hostPos);
+
+      // START
+      this.renderer.addClass(this.tooltip, "ng-tooltip-show");
+    };
+  };
+
+  mouseLeave = () => {
+    if (this.tooltip) {
+      this.renderer.removeClass(this.tooltip, "ng-tooltip-show");
+
+      setTimeout(() => {
+        if (this.tooltip) {
+          this.renderer.removeChild(document.body, this.tooltip);
+        }
+        this.tooltip = null;
+      }, this.delay);
+    }
+  };
+
+  @HostListener("mouseenter") onMouseEnter() {
+    if (!this.allowed) {
+      this.renderer.addClass(this.elementRef.nativeElement, "ng-disabled-button");
+
+      // we have to clear all click listeners of this element.
+      const elementWithoutListeners = this.elementRef.nativeElement.cloneNode(true);
+
+      // calculate hostposition before add event listener;
+      const hostPos = this.elementRef.nativeElement.getBoundingClientRect();
+
+      elementWithoutListeners.addEventListener("mouseenter", this.mouseEnter(hostPos));
+      elementWithoutListeners.addEventListener("mouseleave", this.mouseLeave);
+
+      this.elementRef.nativeElement.replaceWith(elementWithoutListeners);
+    }
+  }
+
+  create() {
+    this.tooltip = this.renderer.createElement("span");
+
+    this.renderer.appendChild(
+      this.tooltip,
+      this.renderer.createText(this.tooltipTitle) // textNode
+    );
+
+    this.renderer.appendChild(document.body, this.tooltip);
+    // this.renderer.appendChild(this.elementRef.nativeElement, this.tooltip);
+
+    this.renderer.addClass(this.tooltip, "ng-tooltip");
+    this.renderer.addClass(this.tooltip, "ng-tooltip-bottom");
+
+    // delay 설정
+    this.renderer.setStyle(this.tooltip, "-webkit-transition", `opacity ${this.delay}ms`);
+    this.renderer.setStyle(this.tooltip, "-moz-transition", `opacity ${this.delay}ms`);
+    this.renderer.setStyle(this.tooltip, "-o-transition", `opacity ${this.delay}ms`);
+    this.renderer.setStyle(this.tooltip, "transition", `opacity ${this.delay}ms`);
+  }
+
+  setPosition(hostPos: DOMRect) {
+    const tooltipPos = this.tooltip.getBoundingClientRect();
+
+    const scrollPos =
+      window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    let top: number, left: number;
+
+    if (this.placement === "top") {
+      top = hostPos.top - tooltipPos.height - this.offset;
+      left = hostPos.left + (hostPos.width - tooltipPos.width) / 2;
+    }
+
+    if (this.placement === "bottom") {
+      top = hostPos.bottom + this.offset;
+      left = hostPos.left + (hostPos.width - tooltipPos.width) / 2;
+    }
+
+    if (this.placement === "left") {
+      top = hostPos.top + (hostPos.height - tooltipPos.height) / 2;
+      left = hostPos.left - tooltipPos.width - this.offset;
+    }
+
+    if (this.placement === "right") {
+      top = hostPos.top + (hostPos.height - tooltipPos.height) / 2;
+      left = hostPos.right + this.offset;
+    }
+
+    this.renderer.setStyle(this.tooltip, "top", `${top + scrollPos}px`);
+    this.renderer.setStyle(this.tooltip, "left", `${left}px`);
   }
 }
