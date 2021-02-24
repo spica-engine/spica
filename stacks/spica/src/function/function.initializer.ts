81996dcc075795965f5946a7d11fe70e90fb930b
htuna07 <h.tuna96@gmail.com>
2021-02-24 14:12:18 +0300

    fix(function): remove developer section if user has no access for function or webhook list

--- stacks/spica/src/function/function.initializer.ts
+++ stacks/spica/src/function/function.initializer.ts
@@ -20,24 +20,62 @@ export class FunctionInitializer {
       this.routeService.dispatch(
         new CherryPickAndRemove(e => e.icon == "memory" && /\/function\//.test(e.path))
       );
+
       funcs.forEach(func => {
-        this.routeService.dispatch(
-          new Upsert({
-            category: RouteCategory.Developer,
-            id: func._id,
-            icon: "memory",
-            path: `/function/${func._id}`,
-            display: func.name
-          })
-        );
+        Promise.all([
+          this.passport.checkAllowed("function:show", func._id).toPromise(),
+          this.passport.checkAllowed("function:update", func._id).toPromise()
+        ]).then(permissions => {
+          if (permissions.every(p => p == true)) {
+            this.routeService.dispatch(
+              new Upsert({
+                category: RouteCategory.Developer,
+                id: func._id,
+                icon: "memory",
+                path: `/function/${func._id}`,
+                display: func.name
+              })
+            );
+          }
+        });
       });
     });
   }
 
   async appInitializer() {
-    const allowed = await this.passport.checkAllowed("function:index", "*").toPromise();
-    if (this.passport.identified && allowed) {
+    if (!this.passport.identified) {
+      return;
+    }
+
+    const [functionIndex, webhookIndex] = await Promise.all([
+      this.passport.checkAllowed("function:index", "*").toPromise(),
+      this.passport.checkAllowed("webhook:index", "*").toPromise()
+    ]);
+
+    if (!functionIndex && !webhookIndex) {
+      this.routeService.dispatch(new RemoveCategory(RouteCategory.Developer));
+      return;
+    }
+
+    if (functionIndex) {
       this.functionService.loadFunctions().toPromise();
+    }
+
+    const [functionLog, webhookLog] = await Promise.all([
+      functionIndex
+        ? this.passport.checkAllowed("function:logs:index").toPromise()
+        : Promise.resolve(false),
+      webhookIndex
+        ? this.passport.checkAllowed("webhook:logs:index").toPromise()
+        : Promise.resolve(false)
+    ]);
+
+    if (!functionLog && !webhookLog) {
+      this.routeService.dispatch(new RemoveCategory(RouteCategory.Developer_Sub));
+    } else if (!functionLog) {
+      this.routeService.dispatch(new CherryPickAndRemove(e => e.id == "list_all_logs"));
+    } else if (!webhookLog) {
+      this.routeService.dispatch(new CherryPickAndRemove(e => e.id == "webhook_logs"));
     }
   }
 }
