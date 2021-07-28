7a897c811a666c0c1e90f4920022c439007acc0a
htuna07 <h.tuna96@gmail.com>
2021-07-28 15:39:54 +0300

    card component update

--- stacks/spica/src/dashboard/components/card/card.component.ts
+++ stacks/spica/src/dashboard/components/card/card.component.ts
@@ -11,17 +11,4 @@ export class CardComponent {
   @Input() componentData$: Observable<any>;
 
   @Output() onUpdate: EventEmitter<object> = new EventEmitter();
-
-  onSubmit(form, button, inputs = []) {
-    const query = {};
-    for (const input of inputs) {
-      query[input.key] = input.value;
-    }
-
-    const params = new HttpParams({fromObject: query});
-    const url = button.target + "?" + params.toString();
-    form.action = url;
-
-    form.submit();
-  }
 }
