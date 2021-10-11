f886e2aa6aed03ac8d289b1733757b1fa11fc34a
htuna07 <h.tuna96@gmail.com>
2021-10-11 17:15:06 +0300

    refactor(dashboard): pass acces token to the card component target url

--- stacks/spica/src/dashboard/components/card/card.component.ts
+++ stacks/spica/src/dashboard/components/card/card.component.ts
@@ -1,4 +1,5 @@
 import {Component, EventEmitter, Input, Output} from "@angular/core";
+import {PassportService} from "@spica-client/passport";
 import {Observable} from "rxjs";
 
 @Component({
@@ -10,4 +11,10 @@ export class CardComponent {
   @Input() componentData$: Observable<any>;
 
   @Output() onUpdate: EventEmitter<object> = new EventEmitter();
+
+  token;
+
+  constructor(private passport: PassportService) {
+    this.token = this.passport.token;
+  }
 }
