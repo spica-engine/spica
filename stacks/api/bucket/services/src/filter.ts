e153e3e391c724a31686fc37f7ed0e0b50165db2
htuna07 <h.tuna96@gmail.com>
2021-12-01 15:37:13 +0300

    perf(bucket): remove conversion of objectid to string from aggregation pipelines (#786)

--- stacks/api/bucket/services/src/filter.ts
+++ stacks/api/bucket/services/src/filter.ts
@@ -81,3 +81,41 @@ export function extractFilterPropertyMap(filter: object) {
   }
   return maps;
 }
+
+export function replaceFilterObjectIds(filter: object) {
+  for (const [key, value] of Object.entries(filter)) {
+    // run recursively for each logical operators such as { $or : [ { expression1 } ,{ expression2 } ] }
+    if (LogicalExtractor.operators.includes(key)) {
+      value.forEach(expression => replaceFilterObjectIds(expression));
+    }
+
+    if (key != "_id" && !key.endsWith("._id")) {
+      continue;
+    }
+
+    // { "_id": { ... } }
+    if (typeof value == "object") {
+      for (let [k, v] of Object.entries(value)) {
+        // { "_id": { $in: [...] } }
+        if (typeof v == "object" && Array.isArray(v)) {
+          value[k] = v.map(id => {
+            return ObjectIdIfValid(id);
+          });
+        }
+        // { "_id": { $eq: "<id>" } }
+        else if (typeof v == "string") {
+          value[k] = ObjectIdIfValid(v);
+        }
+      }
+    }
+    // { "_id": "<id>" }
+    else if (typeof value == "string") {
+      filter[key] = ObjectIdIfValid(value);
+    }
+  }
+  return filter;
+}
+
+function ObjectIdIfValid(val) {
+  return ObjectId.isValid(val) ? new ObjectId(val) : val;
+}
