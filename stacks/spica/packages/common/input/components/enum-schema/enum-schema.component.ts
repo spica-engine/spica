aa640a59228b693f5b42c6d808947a5d77831f7f
htuna07 <h.tuna96@gmail.com>
2021-12-29 14:47:22 +0300

    test fix

--- stacks/spica/packages/common/input/components/enum-schema/enum-schema.component.ts
+++ stacks/spica/packages/common/input/components/enum-schema/enum-schema.component.ts
@@ -53,6 +53,7 @@ export class EnumSchemaComponent implements OnChanges {
   removeItem(index) {
     this.schema.enum.splice(index, 1);
     if (this.schema.enum.length < 1) {
+      // empty enums are not allowed on the api
       delete this.schema.enum;
     }
   }
