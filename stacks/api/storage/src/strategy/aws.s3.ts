287a39255d50c2166f17a143ed59d02a43fbf414
htuna07 <h.tuna96@gmail.com>
2022-09-02 16:23:37 +0300

    feat(storage): aws s3 bucket support

---
+++ stacks/api/storage/src/strategy/aws.s3.ts
@@ -0,0 +1,54 @@
+import {Strategy} from "./strategy";
+import * as AWS from "aws-sdk";
+
+export class AWSS3 implements Strategy {
+  s3: AWS.S3;
+  constructor(private credentialsPath: string, private bucketName: string) {
+    AWS.config.loadFromPath(this.credentialsPath);
+    this.s3 = new AWS.S3();
+  }
+
+  read(id: string): Promise<Buffer> {
+    return new Promise((resolve, reject) => {
+      this.s3.getObject({Bucket: this.bucketName, Key: id}, (err, data) => {
+        if (err) {
+          return reject(err);
+        }
+
+        return resolve(data.Body as Buffer);
+      });
+    });
+  }
+
+  write(id: string, data: Buffer, mimeType?: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.s3.upload(
+        {Bucket: this.bucketName, Key: id, Body: data, ContentType: mimeType},
+        (err, data) => {
+          if (err) {
+            return reject(err);
+          }
+
+          return resolve();
+        }
+      );
+    });
+  }
+
+  delete(id: string): void | Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.s3.deleteObject({Bucket: this.bucketName, Key: id}, (err, data) => {
+        if (err) {
+          return reject(err);
+        }
+
+        return resolve();
+      });
+    });
+  }
+
+  url(id: string): string | Promise<string> {
+    // @TODO: find a way to get object location instead of this
+    return `https://${this.bucketName}.s3.${AWS.config.region}.amazonaws.com/${id}`
+  }
+}
