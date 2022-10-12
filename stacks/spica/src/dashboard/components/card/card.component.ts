318ec079afeff39d5103798261874f9456312c3a
Tuna Küçükertaş <h.tuna96@gmail.com>
2022-10-13 00:42:40 +0300

    feat(dashboard): allow to send json requests for card component (#903)

--- stacks/spica/src/dashboard/components/card/card.component.ts
+++ stacks/spica/src/dashboard/components/card/card.component.ts
@@ -1,6 +1,20 @@
+import {HttpClient} from "@angular/common/http";
 import {Component, EventEmitter, Input, Output} from "@angular/core";
+import {Form} from "@angular/forms";
 import {PassportService} from "@spica-client/passport";
 import {Observable} from "rxjs";
+
+interface CardInput {
+  key: string;
+  type: string;
+  value: any;
+}
+
+interface CardButton {
+  target: string;
+  method: string;
+  enctype: string;
+}
 
 @Component({
   selector: "dashboard-card",
@@ -12,9 +26,28 @@ export class CardComponent {
 
   @Output() onUpdate: EventEmitter<object> = new EventEmitter();
 
-  token;
+  token: string;
 
-  constructor(private passport: PassportService) {
+  constructor(private passport: PassportService, private http: HttpClient) {
     this.token = this.passport.token;
+  }
+
+  async submit(form: HTMLFormElement, inputs: CardInput[], button: CardButton) {
+    if ((!button.enctype || button.enctype == "application/json") && button.method == "post") {
+      const body: any = inputs.reduce((acc, {key, value}) => {
+        acc[key] = value;
+        return acc;
+      }, {});
+
+      const res = await this.http
+        .request<any>(button.method, button.target, {body, headers: {authorization: this.token}})
+        .toPromise()
+        .catch(e => e);
+
+      const tab = window.open("_blank");
+      tab.document.write(JSON.stringify(res));
+    } else {
+      form.submit();
+    }
   }
 }
