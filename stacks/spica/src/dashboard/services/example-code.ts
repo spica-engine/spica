daa95ea8081245d79d9c6d0a20d5f0380f3afe36
kanangsmv <rio.kenan@gmail.com>
2023-03-28 16:38:25 +0300

    table ui

--- stacks/spica/src/dashboard/services/example-code.ts
+++ stacks/spica/src/dashboard/services/example-code.ts
@@ -125,18 +125,18 @@ const example = {
   table: `{
     "title": "table title",
     "data": [
-      { "position": 1, "name": "Hydrogen", "weight": 1.0079, "symbol": "H" },
-      { "position": 2, "name": "Helium", "weight": 4.0026, "symbol": "He" },
-      { "position": 3, "name": "Lithium", "weight": 6.941, "symbol": "Li" },
-      { "position": 4, "name": "Beryllium", "weight": 9.0122, "symbol": "Be" },
-      { "position": 5, "name": "Boron", "weight": 10.811, "symbol": "B" },
-      { "position": 6, "name": "Carbon", "weight": 12.0107, "symbol": "C" },
-      { "position": 7, "name": "Nitrogen", "weight": 14.0067, "symbol": "N" },
-      { "position": 8, "name": "Oxygen", "weight": 15.9994, "symbol": "O" },
-      { "position": 9, "name": "Fluorine", "weight": 18.9984, "symbol": "F" },
-      { "position": 10, "name": "Neon", "weight": 20.1797, "symbol": "Ne" }
+      { "position": 1, "name": "Hydrogen", "weight": 1.0079, "symbol": "H", "asdasd": "asdsqweqwe","aasdssdasd": "asdsqweqasdasde"},
+      { "position": 2, "name": "Helium", "weight": 4.0026, "symbol": "He","asdasd": "asdsqweqwe","aasdssdasd": "asdsqweqasdasde" },
+      { "position": 3, "name": "Lithium", "weight": 6.941, "symbol": "Li","asdasd": "asdsqweqwe","aasdssdasd": "asdsqweqasdasde" },
+      { "position": 4, "name": "Beryllium", "weight": 9.0122, "symbol": "Be","asdasd": "asdsqweqwe","aasdssdasd": "asdsqweqasdasde" },
+      { "position": 5, "name": "Boron", "weight": 10.811, "symbol": "B","asdasd": "asdsqweqwe","aasdssdasd": "asdsqweqasdasde" },
+      { "position": 6, "name": "Carbon", "weight": 12.0107, "symbol": "C", "asdasd": "asdsqweqwe","aasdssdasd": "asdsqweqasdasde" },
+      { "position": 7, "name": "Nitrogen", "weight": 14.0067, "symbol": "N","asdasd": "asdsqweqwe","aasdssdasd": "asdsqweqasdasde" },
+      { "position": 8, "name": "Oxygen", "weight": 15.9994, "symbol": "O","asdasd": "asdsqweqwe","aasdssdasd": "asdsqweqasdasde" },
+      { "position": 9, "name": "Fluorine", "weight": 18.9984, "symbol": "F","asdasd": "asdsqweqwe","aasdssdasd": "asdsqweqasdasde" },
+      { "position": 10, "name": "Neon", "weight": 20.1797, "symbol": "Ne","asdasd": "asdsqweqwe","aasdssdasd": "asdsqweqasdasde" }
     ],
-    "displayedColumns": ["position", "name", "weight", "symbol"],
+    "displayedColumns": ["position", "name", "weight", "symbol","asdasd"],
     "filters": [{ "key": "name", "type": "string", "value": "['Hydrogen','Nitrogen']" }]
   }`,
   card: `{
