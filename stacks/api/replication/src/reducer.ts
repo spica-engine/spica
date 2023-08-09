209027a5fe86afdf866de6bb40ab68ba192d95dc
Spica <Spica>
2023-08-09 12:02:56 +0300

    order of the catch, then

--- stacks/api/replication/src/reducer.ts
+++ stacks/api/replication/src/reducer.ts
@@ -9,6 +9,14 @@ export class JobReducer implements IJobReducer {
   do(meta: JobMeta, job: Function) {
     return this.service._coll
       .updateOne(meta, {$setOnInsert: meta}, {upsert: true})
+      .catch(e => {
+        // If replicas are triggered at the same time(before upsert completion), this error occurs.
+        // for this case, we can ignore the error since we know one of replicas already took the job
+        if (e.code == 11000) {
+          return {upsertedCount: 0};
+        }
+        throw Error(e);
+      })
       .then(({upsertedCount}) => {
         if (!upsertedCount) {
           return false;
@@ -16,14 +24,6 @@ export class JobReducer implements IJobReducer {
 
         job();
         return true;
-      })
-      .catch(e => {
-        // If replicas are triggered at the same time(before upsert completion), this error occurs.
-        // for this case, we can ignore the error since we know one of replicas already took the job
-        if (e.code == 11000) {
-          return false;
-        }
-        throw Error(e);
       });
   }
 }
