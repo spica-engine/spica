bb6a6ef8ab659776d1848e81891b9eb8b9148727
Spica <Spica>
2023-09-21 18:33:51 +0300

    code quality

--- stacks/api/storage/src/body.ts
+++ stacks/api/storage/src/body.ts
@@ -10,7 +10,7 @@ import {
 import {ObjectId} from "@spica-server/database";
 import {raw, json} from "body-parser";
 import {deserialize} from "bson";
-import {Observable} from "rxjs";
+import {Observable, OperatorFunction, PartialObserver, Subscriber, TeardownLogic} from "rxjs";
 import {finalize, switchMapTo} from "rxjs/operators";
 import {StorageOptions, STORAGE_OPTIONS} from "./options";
 import * as multer from "multer";
@@ -18,13 +18,63 @@ import * as fs from "fs";
 // import { Express } from 'express'
 // import { Multer } from 'multer';
 
+// decide whether it should be abstract
+class __BaseBody {
+  payloadSizeError: HttpException;
+  limit: number;
+
+  constructor(public options: StorageOptions) {
+    this.payloadSizeError = new HttpException(
+      `maximum object size is ${this.options.objectSizeLimit}Mi`,
+      HttpStatus.PAYLOAD_TOO_LARGE
+    );
+
+    this.limit = this.options.objectSizeLimit * 1024 * 1024;
+  }
+
+  isLimitExceeded(req) {
+    return Number(req.headers["content-length"]) > this.limit;
+  }
+
+  handleParseError(error, observer) {
+    if (error.type == "entity.too.large") {
+      return observer.error(this.payloadSizeError);
+    }
+    return observer.error(error);
+  }
+
+  buildInterceptor(
+    beforeHandleSubscriber: (subscriber: Subscriber<any>) => TeardownLogic,
+    next: CallHandler,
+    afterHandleOperator?: OperatorFunction<any, any>
+  ) {
+    let obs = new Observable(beforeHandleSubscriber).pipe(switchMapTo(next.handle()));
+    if (afterHandleOperator) {
+      obs = obs.pipe(afterHandleOperator);
+    }
+
+    return obs;
+  }
+
+  completeObserver(observer) {
+    observer.next();
+    observer.complete();
+  }
+}
+
 // minimize copy-paste code
-abstract class __MultipartFormData {
-  constructor(@Inject(STORAGE_OPTIONS) public options: StorageOptions) {}
+abstract class __MultipartFormDataBody extends __BaseBody {
+  constructor(@Inject(STORAGE_OPTIONS) public options: StorageOptions) {
+    super(options);
+  }
+
+  isContentTypeValid(req) {
+    return req.headers["content-type"].startsWith("multipart/form-data");
+  }
 
   fileCleanup(context: ExecutionContext) {
     const [req] = context.getArgs();
-    if (req.headers["content-type"].startsWith("multipart/form-data")) {
+    if (this.isContentTypeValid(req)) {
       return Promise.all(req.body.map(b => fs.promises.unlink(b.path)));
     }
     return Promise.resolve();
@@ -32,21 +82,13 @@ abstract class __MultipartFormData {
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const [req, res] = context.getArgs();
-    return new Observable(observer => {
-      const limit = this.options.objectSizeLimit * 1024 * 1024;
-
-      const payloadSizeError = new HttpException(
-        `maximum object size is ${this.options.objectSizeLimit}Mi`,
-        HttpStatus.PAYLOAD_TOO_LARGE
-      );
-
-      if (Number(req.headers["content-length"]) > limit) {
-        return observer.error(payloadSizeError);
+    const beforeHandleSubscriber = observer => {
+      if (this.isLimitExceeded(req)) {
+        return observer.error(this.payloadSizeError);
       }
 
-      if (!req.headers["content-type"].startsWith("multipart/form-data")) {
-        observer.next();
-        observer.complete();
+      if (!this.isContentTypeValid(req)) {
+        this.completeObserver(observer);
         return;
       }
 
@@ -56,111 +98,104 @@ abstract class __MultipartFormData {
 
       parser(req, res, error => {
         if (error) {
-          if (error.type == "entity.too.large") {
-            return observer.error(payloadSizeError);
-          }
-          return observer.error(error);
+          return this.handleParseError(error, observer);
         }
+
         req.body = req.files;
 
-        observer.next();
-        observer.complete();
+        this.completeObserver(observer);
       });
-    }).pipe(
-      switchMapTo(
-        next.handle().pipe(
-          finalize(() =>
-            // i think multer(or we) still tries to open file, put the cleanup task to the end
-            setImmediate(() => this.fileCleanup(context))
-          )
-        )
-      )
+    };
+
+    const afterHandleOperator = finalize(() =>
+      // i think multer(or we) still tries to open file, put the cleanup task to the end
+      // it happens for only error cases
+      setImmediate(() => this.fileCleanup(context))
     );
+
+    return this.buildInterceptor(beforeHandleSubscriber, next, afterHandleOperator);
   }
 }
 
-abstract class __BsonBody {
-  constructor(@Inject(STORAGE_OPTIONS) public options: StorageOptions) {}
+abstract class __BsonBody extends __BaseBody {
+  constructor(@Inject(STORAGE_OPTIONS) public options: StorageOptions) {
+    super(options);
+  }
+
+  isContentTypeValid(req) {
+    return req.headers["content-type"] == "application/bson";
+  }
+
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const [req, res] = context.getArgs();
-    return new Observable(observer => {
-      const limit = this.options.objectSizeLimit * 1024 * 1024;
-      const parser = raw({
-        type: "application/bson",
-        limit
-      });
 
-      const payloadSizeError = new HttpException(
-        `maximum object size is ${this.options.objectSizeLimit}Mi`,
-        HttpStatus.PAYLOAD_TOO_LARGE
-      );
-
-      if (Number(req.headers["content-length"]) > limit) {
-        return observer.error(payloadSizeError);
+    const beforeHandleSubscriber = observer => {
+      if (this.isLimitExceeded(req)) {
+        return observer.error(this.payloadSizeError);
       }
 
-      if (req.headers["content-type"] != "application/bson") {
-        observer.next();
-        observer.complete();
+      if (!this.isContentTypeValid(req)) {
+        this.completeObserver(observer);
         return;
       }
 
+      const parser = raw({
+        type: "application/bson",
+        limit: this.limit
+      });
+
       parser(req, res, error => {
         if (error) {
-          if (error.type == "entity.too.large") {
-            return observer.error(payloadSizeError);
-          }
-          return observer.error(error);
+          return this.handleParseError(error, observer);
         }
+
         try {
           req.body = deserialize(req.body, {promoteBuffers: true});
         } catch (error) {
           return observer.error(error);
         }
-        observer.next();
-        observer.complete();
       });
-    }).pipe(switchMapTo(next.handle()));
+    };
+
+    return this.buildInterceptor(beforeHandleSubscriber, next);
   }
 }
 
-abstract class __JsonBody {
-  constructor(@Inject(STORAGE_OPTIONS) public options: StorageOptions) {}
+abstract class __JsonBody extends __BaseBody {
+  constructor(@Inject(STORAGE_OPTIONS) public options: StorageOptions) {
+    super(options);
+  }
+
+  isContentTypeValid(req) {
+    return req.headers["content-type"] != "application/json";
+  }
+
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const [req, res] = context.getArgs();
 
-    return new Observable(observer => {
-      const limit = this.options.objectSizeLimit * 1024 * 1024;
+    const beforeHandleSubscriber = observer => {
+      if (this.isLimitExceeded(req)) {
+        return observer.error(this.payloadSizeError);
+      }
+
+      if (!this.isContentTypeValid(req)) {
+        return this.completeObserver(observer);
+      }
+
       const parser = json({
-        limit
+        limit: this.limit
       });
-
-      const payloadSizeError = new HttpException(
-        `maximum object size is ${this.options.objectSizeLimit}Mi`,
-        HttpStatus.PAYLOAD_TOO_LARGE
-      );
-
-      if (Number(req.headers["content-length"]) > limit) {
-        return observer.error(payloadSizeError);
-      }
-
-      if (req.headers["content-type"] != "application/json") {
-        observer.next();
-        observer.complete();
-        return;
-      }
 
       parser(req, res, error => {
         if (error) {
-          if (error.type == "entity.too.large") {
-            return observer.error(payloadSizeError);
-          }
-          return observer.error(error);
+          return this.handleParseError(error, observer);
         }
-        observer.next();
-        observer.complete();
+
+        this.completeObserver(observer);
       });
-    }).pipe(switchMapTo(next.handle()));
+    };
+
+    return this.buildInterceptor(beforeHandleSubscriber, next);
   }
 }
 
@@ -173,7 +208,7 @@ export function JsonBodyParser(): Type<any> {
 }
 
 export function MultipartFormDataParser(): Type<any> {
-  return mixin(class extends __MultipartFormData {});
+  return mixin(class extends __MultipartFormDataBody {});
 }
 
 export interface StorageObject<DataType = Buffer> {
