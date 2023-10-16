d269eb1d71eeb3e85a89869064f69a516ececeea
Spica <Spica>
2023-10-16 16:25:15 +0300

    devkit tests

--- stacks/api/function/packages/storage/test/storage.spec.ts
+++ stacks/api/function/packages/storage/test/storage.spec.ts
@@ -4,6 +4,14 @@ import * as BSON from "bson";
 
 jasmine.getEnv().allowRespy(true);
 
+function getValueOfFormField(form, field) {
+  return form
+    .getBuffer()
+    .toString()
+    .split(form.getBoundary())
+    .find(line => line.includes(`name="${field}"`));
+}
+
 describe("@spica-devkit/Storage", () => {
   let getSpy: jasmine.Spy<any>;
   let postSpy: jasmine.Spy<any>;
@@ -24,69 +32,107 @@ describe("@spica-devkit/Storage", () => {
   });
 
   describe("Storage", () => {
-    const storageObject: Storage.BufferWithMeta = {
-      contentType: "text/plain",
-      name: "my_text.txt",
-      data: "spica"
-    };
+    
 
-    it("should insert storage object", async () => {
-      await Storage.insert(storageObject, onUploadProgress);
-
-      const body = {
-        content: [
-          {
-            name: "my_text.txt",
-            content: {
-              data: new BSON.Binary("spica"),
-              type: "text/plain"
-            }
-          }
-        ]
-      };
-
-      expect(postSpy).toHaveBeenCalledTimes(1);
-      // expect(postSpy).toHaveBeenCalledWith("storage", jsonToArrayBuffer(body), {
-      //   onUploadProgress: onUploadProgress
-      // });
-    });
-
-    it("should insert storage objects", async () => {
-      await Storage.insertMany([storageObject], onUploadProgress);
-
-      const body = {
-        content: [
-          {
-            name: "my_text.txt",
-            content: {
-              data: new BSON.Binary("spica"),
-              type: "text/plain"
-            }
-          }
-        ]
-      };
-
-      expect(postSpy).toHaveBeenCalledTimes(1);
-      // expect(postSpy).toHaveBeenCalledWith("storage", jsonToArrayBuffer(body), {
-      //   onUploadProgress: onUploadProgress
-      // });
-    });
-
-    it("should update storage object", async () => {
-      await Storage.update("storage_object_id", storageObject, onUploadProgress);
-
-      const body = {
+    describe("nodejs", () => {
+      const storageObject: Storage.BufferWithMeta = {
+        contentType: "text/plain",
         name: "my_text.txt",
-        content: {
-          data: new BSON.Binary("spica"),
-          type: "text/plain"
-        }
+        data: "spica"
       };
 
-      expect(putSpy).toHaveBeenCalledTimes(1);
-      // expect(putSpy).toHaveBeenCalledWith("storage/storage_object_id", jsonToArrayBuffer(body), {
-      //   onUploadProgress: onUploadProgress
-      // });
+      
+      it("should insert storage object", async () => {
+        await Storage.insert(storageObject, onUploadProgress);
+
+        expect(postSpy).toHaveBeenCalledTimes(1);
+        const [path, formData, options] = postSpy.calls.allArgs()[0];
+        expect(path).toEqual("storage");
+
+        expect(getValueOfFormField(formData, "my_text.txt").includes("spica")).toEqual(true);
+
+        expect((options as any).onUploadProgress).toEqual(onUploadProgress);
+        expect((options as any).headers["content-type"].split(";")[0]).toEqual(
+          "multipart/form-data"
+        );
+      });
+
+      it("should insert storage objects", async () => {
+        const storageObject2 = {
+          contentType: "application/json",
+          name: "languages.json",
+          data: "{'test':123}"
+        }
+        await Storage.insertMany([storageObject,storageObject2], onUploadProgress);
+
+        expect(postSpy).toHaveBeenCalledTimes(1);
+        const [path, formData, options] = postSpy.calls.allArgs()[0];
+        expect(path).toEqual("storage");
+
+        expect(getValueOfFormField(formData, "my_text.txt").includes("spica")).toEqual(true);
+        expect(getValueOfFormField(formData, "languages.json").includes("{'test':123}")).toEqual(true);
+
+
+        expect((options as any).onUploadProgress).toEqual(onUploadProgress);
+        expect((options as any).headers["content-type"].split(";")[0]).toEqual(
+          "multipart/form-data"
+        );
+      });
+
+      it("should update storage object", async () => {
+        await Storage.update("storage_object_id", storageObject, onUploadProgress);
+
+        expect(putSpy).toHaveBeenCalledTimes(1);
+
+        const [path, formData, options] = putSpy.calls.allArgs()[0];
+        expect(path).toEqual("storage/storage_object_id");
+
+        expect(getValueOfFormField(formData, "my_text.txt").includes("spica")).toEqual(true);
+
+        expect((options as any).onUploadProgress).toEqual(onUploadProgress);
+        expect((options as any).headers["content-type"].split(";")[0]).toEqual(
+          "multipart/form-data"
+        );
+      });
+
+      it("should download storage object", () => {
+        Storage.download("storage_object_id", {
+          onDownloadProgress
+        });
+
+        expect(getSpy).toHaveBeenCalledTimes(1);
+        expect(getSpy).toHaveBeenCalledWith("storage/storage_object_id/view", {
+          headers: undefined,
+          onDownloadProgress,
+          responseType: "stream"
+        });
+      });
+    });
+
+    describe("browser", () => {
+      beforeEach(() => {
+        global.window = {} as any;
+      });
+
+      afterEach(() => {
+        delete global.window;
+      });
+
+      // we can't write tests for insert and update caseson test environment(NodeJS)
+      // because we don't have native FormData library(built-in package for browsers) 
+
+      it("should download storage object", () => {
+        Storage.download("storage_object_id", {
+          onDownloadProgress
+        });
+  
+        expect(getSpy).toHaveBeenCalledTimes(1);
+        expect(getSpy).toHaveBeenCalledWith("storage/storage_object_id/view", {
+          headers: undefined,
+          onDownloadProgress,
+          responseType: "blob"
+        });
+      });
     });
 
     it("should delete storage object", () => {
@@ -110,34 +156,6 @@ describe("@spica-devkit/Storage", () => {
       expect(getSpy).toHaveBeenCalledWith("storage/storage_object_id");
     });
 
-    it("should download storage object for nodejs env", () => {
-      Storage.download("storage_object_id", {
-        onDownloadProgress
-      });
-
-      expect(getSpy).toHaveBeenCalledTimes(1);
-      expect(getSpy).toHaveBeenCalledWith("storage/storage_object_id/view", {
-        headers: undefined,
-        onDownloadProgress,
-        responseType: "stream"
-      });
-    });
-
-    it("should download storage object for browser env", () => {
-      global.window = {} as any;
-
-      Storage.download("storage_object_id", {
-        onDownloadProgress
-      });
-
-      expect(getSpy).toHaveBeenCalledTimes(1);
-      expect(getSpy).toHaveBeenCalledWith("storage/storage_object_id/view", {
-        headers: undefined,
-        onDownloadProgress,
-        responseType: "blob"
-      });
-
-      global.window = undefined;
-    });
+    
   });
 });
