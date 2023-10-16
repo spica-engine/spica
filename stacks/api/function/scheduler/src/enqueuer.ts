d91490b7fae700d6b0ab8046c0ede10c92dd6167
Spica <Spica>
2023-10-16 14:29:49 +0300

    lint

--- stacks/api/function/scheduler/src/enqueuer.ts
+++ stacks/api/function/scheduler/src/enqueuer.ts
@@ -1,9 +1,11 @@
 import {Enqueuer} from "@spica-server/function/enqueuer";
 import {EventQueue, Queue} from "@spica-server/function/queue";
-import { ClassCommander, JobReducer } from "@spica-server/replication";
+import {ClassCommander, JobReducer} from "@spica-server/replication";
 
 export const ENQUEUER = Symbol.for("SCHEDULER_ENQUEUER");
 
 export type EnqueuerFactory<QueueType, OptionsT> = (
-  queue: EventQueue,jobReducer?:JobReducer,commander?:ClassCommander
+  queue: EventQueue,
+  jobReducer?: JobReducer,
+  commander?: ClassCommander
 ) => {queue: Queue<QueueType>; enqueuer: Enqueuer<OptionsT>};
