5aee5f35f7ead7c6a6df75d54e3116a14bfdf403
Mavliut Fozliev <mavliut.fozliev@gmail.com>
2024-10-03 13:46:17 +0300

    fix: build worker using webpack

--- apps/api/webpack.worker.config.js
+++ apps/api/webpack.worker.config.js
@@ -3,13 +3,11 @@ const {NxAppWebpackPlugin} = require("@nx/webpack/app-plugin");
 module.exports = {
   plugins: [
     new NxAppWebpackPlugin({
-      target: "node",
       compiler: "tsc",
       tsConfig: "./tsconfig.json",
       optimization: false,
       outputHashing: "none",
-      generatePackageJson: true,
-      sourceMap: true
+      generatePackageJson: false
     })
   ]
 };
