a16e4597c941843af0063667a881a0103b2e3268
Mavliut Fozliev <128398090+mavliut-fozliev@users.noreply.github.com>
2024-12-20 14:35:03 +0300

    test: add configuration for api/bucket/common (#1039)

--- apps/api/src/bucket/common/test/locale.spec.ts
+++ apps/api/src/bucket/common/test/locale.spec.ts
@@ -1,7 +1,7 @@
 import {provideLanguageFinalizer} from "@spica-server/bucket/common";
 
 describe("provideLanguageChangeUpdater", () => {
-  let childrenSpy: jest.Mock;
+  let childrenSpy: jest.SpyInstance;
   const translatableBuckets = [
     {_id: "bucket1", properties: {title: {}, description: {}}},
     {_id: "bucket2", properties: {name: {}}}
@@ -25,7 +25,7 @@ describe("provideLanguageChangeUpdater", () => {
   });
 
   afterEach(() => {
-    childrenSpy.mockReset();
+    childrenSpy.mockClear();
   });
 
   it("should return updater function", () => {
@@ -109,10 +109,7 @@ describe("provideLanguageChangeUpdater", () => {
     ]);
 
     expect(childrenSpy).toHaveBeenCalledTimes(2);
-    expect(childrenSpy.mock.calls).toEqual([
-      [translatableBuckets[0]],
-      [translatableBuckets[1]]
-    ]);
+    expect(childrenSpy.mock.calls).toEqual([[translatableBuckets[0]], [translatableBuckets[1]]]);
 
     expect(bucketDataService.updateMany).toHaveBeenCalledTimes(2);
     expect(bucketDataService.updateMany.mock.calls).toEqual([
