288e2a9fa532c26bcadf59d089dd8b322c3fdaa5
Spica <Spica>
2025-02-27 15:19:44 +0300

    github release workflow

--- scripts/decide_next_tag.sh
+++ scripts/decide_next_tag.sh
@@ -11,22 +11,22 @@ COMMITS=$(git log "$LATEST_TAG"..HEAD --oneline)
 
 # 4. If there are more than one commit, check the commit messages
 if [ $(echo "$COMMITS" | wc -l) -gt 1 ]; then
-    NEXT_VERSION_TARGET="patch" # Default is patch
+    NEXT_TAG_TARGET="patch" # Default is patch
     while IFS= read -r commit; do
         COMMIT_MESSAGE=$(echo "$commit" | cut -d ' ' -f2-)
         # 5. If any commit message starts with "feat", set next version to minor
         if [[ "$COMMIT_MESSAGE" == feat* ]]; then
-            NEXT_VERSION_TARGET="minor"
+            NEXT_TAG_TARGET="minor"
             break
         fi
     done <<<"$COMMITS"
 
     # 6. Increase the version based on the next version (minor or patch)
     # Get the current version from the latest tag
-    VERSION=$(echo "$LATEST_TAG" | sed 's/^v//') # Remove "v" if present
-    IFS='.' read -r MAJOR MINOR PATCH <<<"$VERSION"
+    LATEST_VERSION=$(echo "$LATEST_TAG" | sed 's/^v//') # Remove "v" if present
+    IFS='.' read -r MAJOR MINOR PATCH <<<"$LATEST_VERSION"
 
-    if [ "$NEXT_VERSION_TARGET" == "minor" ]; then
+    if [ "$NEXT_TAG_TARGET" == "minor" ]; then
         MINOR=$((MINOR + 1))
         PATCH=0 # Reset patch version for minor updates
     else
@@ -34,9 +34,9 @@ if [ $(echo "$COMMITS" | wc -l) -gt 1 ]; then
     fi
 
     # 7. Create the new version tag
-    NEW_VERSION="$MAJOR.$MINOR.$PATCH"
-    export NEXT_VERSION="$NEW_VERSION" # Export the version for use in the next script
-    echo "Next version: $NEW_VERSION"
+    NEW_TAG="$MAJOR.$MINOR.$PATCH"
+    export NEXT_TAG="$NEW_TAG" # Export the version for use in the next script
+    echo "Next tag: $NEXT_TAG"
 else
     echo "No commits found between the latest tag and HEAD."
 fi
