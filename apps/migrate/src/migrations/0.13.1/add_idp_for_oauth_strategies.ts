c681a7d38af5fc0c03f72baa1a9db03f51578f8b
Mavliut Fozliev <mavliut.fozliev@gmail.com>
2025-03-05 11:07:13 +0300

    refactor(migrate): combine find and update methods

--- apps/migrate/src/migrations/0.13.1/add_idp_for_oauth_strategies.ts
+++ apps/migrate/src/migrations/0.13.1/add_idp_for_oauth_strategies.ts
@@ -2,17 +2,6 @@ import {Context} from "../../migrate";
 
 export default async function (ctx: Context) {
   const coll = ctx.database.collection("strategy");
-  const strategies = await coll.find({type: "oauth"}).toArray();
 
-  const bulkOps = [];
-
-  for (let strategy of strategies) {
-    bulkOps.push({
-      updateOne: {filter: {_id: strategy._id}, update: {$set: {"options.idp": "custom"}}}
-    });
-  }
-
-  if (bulkOps.length) {
-    await coll.bulkWrite(bulkOps);
-  }
+  await coll.updateMany({type: "oauth"}, {$set: {"options.idp": "custom"}});
 }
