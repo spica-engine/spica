5e0d918ba8330ea2bfc3a8f3fdb35e4ca5f3c012
Mavliut Fozliev <128398090+mavliut-fozliev@users.noreply.github.com>
2025-03-07 16:15:54 +0300

    feat(api): implement github oauth strategy (#1219)

--- apps/api/src/passport/src/strategy/services/oauth/oauth.service.ts
+++ apps/api/src/passport/src/strategy/services/oauth/oauth.service.ts
@@ -2,6 +2,7 @@ import {PassportOptions, RequestService} from "@spica-server/passport/src/option
 import {StrategyService} from "../strategy.service";
 import {CustomOAuthService} from "./custom";
 import {GoogleOAuthService} from "./google";
+import {GithubOAuthService} from "./github";
 import {FacebookOAuthService} from "./facebook";
 
 export function initializeOAuthServices(
@@ -12,6 +13,7 @@ export function initializeOAuthServices(
   return [
     new CustomOAuthService(strategyService, options, req),
     new GoogleOAuthService(strategyService, options, req),
+    new GithubOAuthService(strategyService, options, req),
     new FacebookOAuthService(strategyService, options, req)
   ];
 }
