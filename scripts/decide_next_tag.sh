e48d19e8739c36c9d3ccd888e839df2ab6785da8
Tuna Küçükertaş <h.tuna96@gmail.com>
2025-03-13 13:32:56 +0300

    ci: increase selectivity of deciding next tag (#1227)

--- scripts/decide_next_tag.sh
+++ scripts/decide_next_tag.sh
@@ -7,22 +7,34 @@ git fetch origin master
 LATEST_TAG=$(git describe --tags --abbrev=0)
 
 # 3. Get the commits between the latest tag and the latest commit
-COMMITS=$(git log "$LATEST_TAG"..HEAD --oneline)
+COMMITS=$(git log "$LATEST_TAG"..HEAD --oneline || echo "")
 
-# 4. If there are more than one commit, check the commit messages
-if [ $(echo "$COMMITS" | wc -l) -gt 1 ]; then
-    NEXT_TAG_TARGET="patch" # Default is patch
-    while IFS= read -r commit; do
-        COMMIT_MESSAGE=$(echo "$commit" | cut -d ' ' -f2-)
-        # 5. If any commit message starts with "feat", set next version to minor
-        if [[ "$COMMIT_MESSAGE" == feat* ]]; then
-            NEXT_TAG_TARGET="minor"
-            break
-        fi
-    done <<<"$COMMITS"
+# 4. Initialize the next tag target
+NEXT_TAG_TARGET=""
 
-    # 6. Increase the version based on the next version (minor or patch)
-    # Get the current version from the latest tag
+# 5. Check if there are relevant commits
+while IFS= read -r commit; do
+    COMMIT_MESSAGE=$(echo "$commit" | cut -d ' ' -f2-)
+    if [[ "$COMMIT_MESSAGE" == ci* || 
+          "$COMMIT_MESSAGE" == chore* || 
+          "$COMMIT_MESSAGE" == docs* || 
+          "$COMMIT_MESSAGE" == refactor* || 
+          "$COMMIT_MESSAGE" == test* || 
+          "$COMMIT_MESSAGE" == build* || 
+          "$COMMIT_MESSAGE" == style* ]]; then
+        continue
+    fi
+    
+    if [[ "$COMMIT_MESSAGE" == feat* || "$COMMIT_MESSAGE" == deprecate* ]]; then
+        NEXT_TAG_TARGET="minor"
+        break
+    elif [[ "$COMMIT_MESSAGE" == fix* || "$COMMIT_MESSAGE" == perf* ]]; then
+        NEXT_TAG_TARGET="patch"
+    fi
+done <<<"$COMMITS"
+
+# 6. If a relevant commit was found, generate the next tag
+if [ -n "$NEXT_TAG_TARGET" ]; then
     LATEST_VERSION=$(echo "$LATEST_TAG" | sed 's/^v//') # Remove "v" if present
     IFS='.' read -r MAJOR MINOR PATCH <<<"$LATEST_VERSION"
 
@@ -38,5 +50,5 @@ if [ $(echo "$COMMITS" | wc -l) -gt 1 ]; then
     export NEXT_TAG="$NEW_TAG" # Export the version for use in the next script
     echo "Next tag: $NEXT_TAG"
 else
-    echo "No commits found between the latest tag and HEAD."
+    echo "No valuable commit was found to bump the tag."
 fi
