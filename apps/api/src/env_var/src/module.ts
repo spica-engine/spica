65d64406aec706b1d279bb38af9c762dea9e6f6f
Spica <Spica>
2025-03-17 10:59:36 +0300

    implement empty VC synchronizer

--- apps/api/src/env_var/src/module.ts
+++ apps/api/src/env_var/src/module.ts
@@ -1,8 +1,15 @@
-import {Module} from "@nestjs/common";
+import {Inject, Module, Optional} from "@nestjs/common";
 import {EnvVarsService, ServicesModule} from "@spica-server/env_var/services";
 import {EnvVarsController} from "./controller";
 import {SchemaModule} from "@spica-server/core/schema";
 import EnvVarsSchema from "./schema.json" with {type: "json"};
+import {
+  REGISTER_VC_SYNC_PROVIDER,
+  RegisterSyncProvider,
+  VC_REP_MANAGER
+} from "@spica-server/versioncontrol";
+import {IRepresentativeManager} from "@spica-server/interface/representative";
+import {getVCSyncProvider} from "./versioncontrol/env-var";
 
 @Module({})
 export class EnvVarsModule {
@@ -19,5 +26,15 @@ export class EnvVarsModule {
       providers: [EnvVarsService],
       exports: []
     };
+  }
+
+  constructor(
+    @Optional() @Inject(VC_REP_MANAGER) private vcRepManager: IRepresentativeManager,
+    @Optional() @Inject(REGISTER_VC_SYNC_PROVIDER) registerVCSyncProvider: RegisterSyncProvider
+  ) {
+    if (registerVCSyncProvider) {
+      const provider = getVCSyncProvider(this.vcRepManager);
+      registerVCSyncProvider(provider);
+    }
   }
 }
