d80853f542b3ccac2968327d7290c473b8b9b3d4
Mavliut Fozliev <mavliut.fozliev@gmail.com>
2025-03-24 11:49:53 +0300

    chore(migrate): add migration script for function env vars

---
+++ apps/migrate/src/migrations/0.16.0/replace_env_vars.ts
@@ -0,0 +1,25 @@
+import {Context} from "../../migrate";
+
+export default async function (ctx: Context) {
+  const functionCollection = ctx.database.collection("function");
+  const functions = await functionCollection.find({}).toArray();
+
+  const envVarCollection = ctx.database.collection("env_var");
+
+  const bulkOps = [];
+
+  for (let func of functions) {
+    const envVars = [];
+    Object.entries(func.env_vars).forEach(([key, value]) => envVars.push({key, value}));
+
+    if (envVars.length) {
+      await envVarCollection.insertMany(envVars);
+    }
+
+    bulkOps.push({updateOne: {filter: {_id: func._id}, update: {$set: {env_vars: envVars}}}});
+  }
+
+  if (bulkOps.length) {
+    await functionCollection.bulkWrite(bulkOps);
+  }
+}
---
+++ apps/migrate/test/migrations/0_16_0_replace_env_vars.spec.ts
@@ -0,0 +1,176 @@
+import {Db, getConnectionUri, getDatabaseName, start} from "@spica-server/database/testing";
+import color from "cli-color/lib/supports-color";
+import {run} from "@spica/migrate";
+import path from "path";
+
+process.env.TESTONLY_MIGRATION_LOOKUP_DIR = path.join(process.cwd(), "dist/apps/migrate/src");
+
+describe("Replace env vars", () => {
+  let db: Db;
+  let args: string[];
+
+  beforeAll(() => {
+    color.disableColor();
+  });
+
+  beforeEach(async () => {
+    const connection = await start("replset");
+    args = ["--database-uri", await getConnectionUri(), "--database-name", getDatabaseName()];
+    db = connection.db(args[3]);
+
+    await db.collection("function").insertMany([
+      {
+        name: "newFuncJs",
+        language: "javascript",
+        timeout: 6,
+        triggers: {
+          default: {
+            type: "http",
+            active: true,
+            options: {
+              method: "Get",
+              path: "/new-func-js",
+              preflight: true
+            }
+          }
+        },
+        env_vars: {
+          SECRET: "123",
+          APIKEY: "OBJECT_ID"
+        }
+      },
+      {
+        name: "brandNewFuncJs",
+        language: "javascript",
+        timeout: 6,
+        triggers: {
+          default: {
+            type: "http",
+            active: true,
+            options: {
+              method: "Get",
+              path: "/brand-new-func-js",
+              preflight: true
+            }
+          }
+        },
+        env_vars: {}
+      },
+      {
+        name: "newFuncTs",
+        language: "typescript",
+        timeout: 6,
+        triggers: {
+          default: {
+            type: "http",
+            active: true,
+            options: {
+              method: "Get",
+              path: "/new-func-ts",
+              preflight: true
+            }
+          }
+        },
+        env_vars: {
+          SECRET: "123"
+        }
+      }
+    ]);
+  });
+
+  it("should insert env vars", async () => {
+    await run([...args, "--from", "0.15.0", "--to", "0.16.0", "--continue-if-versions-are-equal"]);
+    const envVars = await db
+      .collection("env_var")
+      .find({})
+      .map(obj => {
+        delete obj._id;
+        return obj;
+      })
+      .toArray();
+
+    expect(envVars).toEqual([
+      {
+        key: "SECRET",
+        value: "123"
+      },
+      {
+        key: "APIKEY",
+        value: "OBJECT_ID"
+      },
+      {
+        key: "SECRET",
+        value: "123"
+      }
+    ]);
+  });
+
+  it("should replace env vars", async () => {
+    await run([...args, "--from", "0.15.0", "--to", "0.16.0", "--continue-if-versions-are-equal"]);
+    const functions = await db
+      .collection("function")
+      .find({})
+      .map(obj => {
+        delete obj._id;
+        return obj;
+      })
+      .toArray();
+
+    expect(functions).toEqual([
+      {
+        name: "newFuncJs",
+        language: "javascript",
+        timeout: 6,
+        triggers: {
+          default: {
+            type: "http",
+            active: true,
+            options: {
+              method: "Get",
+              path: "/new-func-js",
+              preflight: true
+            }
+          }
+        },
+        env_vars: [
+          {_id: functions[0].env_vars[0]._id, key: "SECRET", value: "123"},
+          {_id: functions[0].env_vars[1]._id, key: "APIKEY", value: "OBJECT_ID"}
+        ]
+      },
+      {
+        name: "brandNewFuncJs",
+        language: "javascript",
+        timeout: 6,
+        triggers: {
+          default: {
+            type: "http",
+            active: true,
+            options: {
+              method: "Get",
+              path: "/brand-new-func-js",
+              preflight: true
+            }
+          }
+        },
+        env_vars: []
+      },
+      {
+        name: "newFuncTs",
+        language: "typescript",
+        timeout: 6,
+        triggers: {
+          default: {
+            type: "http",
+            active: true,
+            options: {
+              method: "Get",
+              path: "/new-func-ts",
+              preflight: true
+            }
+          }
+        },
+        env_vars: [{_id: functions[2].env_vars[0]._id, key: "SECRET", value: "123"}]
+      }
+    ]);
+  });
+});
