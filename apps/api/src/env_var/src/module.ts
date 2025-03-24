39e2f30996dd5be3eebe9bd8f5b36a8fdbaacabf
Mavliut Fozliev <mavliut.fozliev@gmail.com>
2025-03-24 18:20:15 +0300

    create and register asset handler

--- apps/api/src/env_var/src/module.ts
+++ apps/api/src/env_var/src/module.ts
@@ -1,7 +1,7 @@
 import {Inject, Module, Optional} from "@nestjs/common";
 import {EnvVarsService, ServicesModule} from "@spica-server/env_var/services";
 import {EnvVarsController} from "./controller";
-import {SchemaModule} from "@spica-server/core/schema";
+import {SchemaModule, Validator} from "@spica-server/core/schema";
 import EnvVarsSchema from "./schema.json" with {type: "json"};
 import {
   REGISTER_VC_SYNC_PROVIDER,
@@ -10,6 +10,8 @@ import {
 } from "@spica-server/versioncontrol";
 import {IRepresentativeManager} from "@spica-server/interface/representative";
 import {getVCSyncProvider} from "./versioncontrol/schema";
+import {registerAssetHandlers} from "./asset";
+import {ASSET_REP_MANAGER} from "@spica-server/asset/src/interface";
 
 @Module({})
 export class EnvVarsModule {
@@ -31,11 +33,14 @@ export class EnvVarsModule {
   constructor(
     evs: EnvVarsService,
     @Optional() @Inject(VC_REP_MANAGER) private vcRepManager: IRepresentativeManager,
-    @Optional() @Inject(REGISTER_VC_SYNC_PROVIDER) registerVCSyncProvider: RegisterSyncProvider
+    @Optional() @Inject(REGISTER_VC_SYNC_PROVIDER) registerVCSyncProvider: RegisterSyncProvider,
+    @Optional() @Inject(ASSET_REP_MANAGER) private assetRepManager: IRepresentativeManager,
+    validator: Validator
   ) {
     if (registerVCSyncProvider) {
       const provider = getVCSyncProvider(evs, this.vcRepManager);
       registerVCSyncProvider(provider);
     }
+    registerAssetHandlers(evs, validator, this.assetRepManager);
   }
 }
