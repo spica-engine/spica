2d0178fe3d20b9335f4b8bb7a7ec1fb889115931
Tuna Küçükertaş <h.tuna96@gmail.com>
2025-03-26 14:04:31 +0300

    test(api): add batch module tests (#1239)

--- apps/api/src/batch/src/controller.ts
+++ apps/api/src/batch/src/controller.ts
@@ -11,6 +11,7 @@ import {
   Response
 } from "./interface";
 import {AuthGuard} from "@spica-server/passport/guard";
+import {getBaseUrl, handleResponse, splitIntoChunks} from "./utilities";
 
 @Controller("batch")
 export class BatchController {
@@ -25,14 +26,13 @@ export class BatchController {
     @Body(Schema.validate("http://spica.internal/batch")) batch: BatchRequest,
     @Req() req
   ) {
-    this.httpService.baseURL = this.getBaseUrl(req);
-
-    const requestChunks = this.spliceToChunks(batch.requests, batch.concurrency);
+    this.httpService.baseURL = getBaseUrl(req, this.options);
+    const requestChunks = splitIntoChunks(batch.requests, batch.concurrency);
 
     const responses: Response[] = [];
     for (let requestChunk of requestChunks) {
       const responseHandler = (request, response) =>
-        responses.push(this.handleResponse(request, response));
+        responses.push(handleResponse(request, response));
 
       await Promise.all(
         requestChunk.map(r =>
@@ -44,32 +44,6 @@ export class BatchController {
       );
     }
 
-    return responses;
-  }
-
-  private handleResponse(request: Request, response: HTTPResponse): Response {
-    return {
-      id: request.id,
-      status: response.status,
-      headers: response.headers,
-      body: response.body
-    };
-  }
-
-  private spliceToChunks<T>(items: T[], limit: number) {
-    let chunks: T[][] = [];
-    if (limit > 0) {
-      while (items.length) {
-        chunks.push(items.splice(0, limit));
-      }
-    } else {
-      chunks = [items];
-    }
-
-    return chunks;
-  }
-
-  private getBaseUrl(req) {
-    return `${req.protocol}://localhost:${this.options.port}`;
+    return {responses};
   }
 }
