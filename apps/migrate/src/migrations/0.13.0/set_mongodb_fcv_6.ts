9ced456304ca294a18657c1d259c84845741511d
Spica <Spica>
2025-04-21 15:15:28 +0300

    fix migration issues

--- apps/migrate/src/migrations/0.13.0/set_mongodb_fcv_6.ts
+++ apps/migrate/src/migrations/0.13.0/set_mongodb_fcv_6.ts
@@ -3,6 +3,7 @@ import {Context} from "../../migrate";
 export default async function (ctx: Context) {
   const admin = ctx.database.admin();
   await admin.command({
-    setFeatureCompatibilityVersion: "6.0"
+    setFeatureCompatibilityVersion: "6.0",
+    confirm: true
   });
 }
