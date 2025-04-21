cafc8886a63297b220d453e421b97156d3242cff
Spica <Spica>
2025-04-21 12:58:52 +0300

    remove migration for 0.10.0

--- apps/migrate/src/migrations/0.10.0/set_mongodb_fcv_4_4.ts
+++
@@ -1,8 +0,0 @@
-import {Context} from "../../migrate";
-
-export default async function (ctx: Context) {
-  const admin = ctx.database.admin();
-  await admin.command({
-    setFeatureCompatibilityVersion: "4.4"
-  });
-}
