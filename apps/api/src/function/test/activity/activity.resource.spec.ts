d4604378fecc53b56cb6826d7a8a822b1ecd31f9
Mavliut Fozliev <mavliut.fozliev@gmail.com>
2025-04-21 10:27:29 +0300

    fix Action import

--- apps/api/src/function/test/activity/activity.resource.spec.ts
+++ apps/api/src/function/test/activity/activity.resource.spec.ts
@@ -1,4 +1,4 @@
-import {Action} from "@spica-server/activity/services";
+import {Action} from "@spica-server/interface/activity";
 import {
   createFunctionActivity,
   createFunctionDependencyActivity,
