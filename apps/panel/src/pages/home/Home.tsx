69eedc7d6653bfc03c0e11b21625850c2730b66e
Mozan0 <mehmet.gol@std.antalya.edu.tr>
2025-05-27 11:16:20 +0300

    refactor(sideBar): create a layout for sidebar display for all the pages

--- apps/panel/src/pages/home/Home.tsx
+++ apps/panel/src/pages/home/Home.tsx
@@ -1,41 +1,24 @@
-import React, {memo, useState} from "react";
+import React, {memo} from "react";
 import styles from "./Home.module.scss";
 import {FlexElement} from "oziko-ui-kit";
 import "oziko-ui-kit/dist/index.css";
-import SideBar from "../../components/organisms/sidebar/SideBar";
 import Toolbar from "../../components/atoms/toolbar/Toolbar";
 import VideoDisplay from "../../components/molecules/video-display/VideoDisplay";
 import Quicklinks from "../../components/molecules/quicklinks/Quicklinks";
 import WelcomeText from "../../components/atoms/welcome-text/WelcomeText";
-import {menuItems, navigatorItems, token, name} from "./mock";
+import {token, name} from "./mock";
 
 const Home = () => {
-  const [navigatorOpen, setNavigatorOpen] = useState(true);
-
   return (
     <div className={styles.container}>
-      <SideBar
-        menuItems={menuItems}
-        navigatorItems={navigatorItems}
-        onNavigatorToggle={setNavigatorOpen}
-      />
-      <div
-        className={`${styles.content} ${navigatorOpen ? styles.navigatorOpen : styles.navigatorClosed}`}
-      >
-        <Toolbar token={token} name={name}></Toolbar>
-        <FlexElement
-          dimensionX={"fill"}
-          direction="vertical"
-          gap={10}
-          className={styles.mainContainer}
-        >
-          <WelcomeText />
-          <FlexElement dimensionX={"fill"}>
-            <Quicklinks />
-            <VideoDisplay />
-          </FlexElement>
+      <Toolbar token={token} name={name} />
+      <FlexElement dimensionX="fill" direction="vertical" gap={10} className={styles.content}>
+        <WelcomeText />
+        <FlexElement dimensionX="fill">
+          <Quicklinks />
+          <VideoDisplay />
         </FlexElement>
-      </div>
+      </FlexElement>
     </div>
   );
 };
