f3e42eea6d99a0857601a0589c49c66b0d8575a6
Mozan0 <mehmet.gol@std.antalya.edu.tr>
2025-06-02 13:15:23 +0300

    rebase commit

--- apps/panel/src/layout/Layout.tsx
+++ apps/panel/src/layout/Layout.tsx
@@ -3,20 +3,44 @@ import {Outlet} from "react-router-dom";
 import SideBar from "../components/organisms/sidebar/SideBar";
 import {menuItems, navigatorItems} from "../pages/home/mock";
 import styles from "./Layout.module.scss";
+import {useUIContext} from "../context/UIContext";
+import {Drawer} from "oziko-ui-kit";
 
 const Layout = () => {
   const [navigatorOpen, setNavigatorOpen] = useState(true);
+  const {isSmallScreen, isDrawerOpen, closeDrawer} = useUIContext();
+
+  const sideBarElement = (
+    <SideBar
+      menuItems={menuItems}
+      navigatorItems={navigatorItems}
+      onNavigatorToggle={setNavigatorOpen}
+    />
+  );
 
   return (
     <>
-      <SideBar
-        menuItems={menuItems}
-        navigatorItems={navigatorItems}
-        onNavigatorToggle={setNavigatorOpen}
-      />
+      {isSmallScreen ? (
+        <Drawer
+          placement="left"
+          showCloseButton={false}
+          isOpen={isDrawerOpen}
+          onClose={closeDrawer}
+          size={260}
+        >
+          {sideBarElement}
+        </Drawer>
+      ) : (
+        sideBarElement
+      )}
+
       <div
         className={`${styles.content} ${
-          navigatorOpen ? styles.navigatorOpen : styles.navigatorClosed
+          isSmallScreen
+            ? styles.smallScreen
+            : navigatorOpen
+              ? styles.navigatorOpen
+              : styles.navigatorClosed
         }`}
       >
         <Outlet />
--- apps/panel/src/main.tsx
+++ apps/panel/src/main.tsx
@@ -1,14 +1,16 @@
 import {StrictMode} from "react";
-import {BrowserRouter} from "react-router-dom";
 import * as ReactDOM from "react-dom/client";
-import App from "./app/app";
 import AppRouter from "./router";
 import "oziko-ui-kit/dist/index.css";
 import "./styles.scss";
+import {UIProvider} from "./context/UIContext";
+
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
   <StrictMode>
-    <AppRouter />
+    <UIProvider>
+      <AppRouter />
+    </UIProvider>
   </StrictMode>
 );
