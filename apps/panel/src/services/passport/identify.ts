5d52b359b1b9a8a3980facaed1411ff0a56d3d97
Mehmet Ozan GÖL <125906748+Mozan0@users.noreply.github.com>
2025-06-11 13:49:16 +0300

    feat(login): add login api validation integration (#1333)

---
+++ apps/panel/src/services/passport/identify.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+export type IdentifyParams = {
+  identifier: string;
+  password: string;
+};
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+const apiClient = axios.create({
+  baseURL: BASE_URL
+});
+
+export async function authorization(
+  identityOrStrategy: IdentifyParams | string,
+  openCallback?: (url: string) => void
+): Promise<any> {
+  if (typeof identityOrStrategy !== "string") {
+    return apiClient.post(`/api/passport/identify`, identityOrStrategy);
+  }
+  //Login with strategy part
+  const strategy = identityOrStrategy;
+  const strategyRes = await apiClient.get(`/api/passport/strategy/${strategy}/url`, {
+    params: {identityOrStrategy: strategy}
+  });
+
+  if (openCallback) openCallback(strategyRes.data.url);
+
+  return apiClient.get("/api/passport/identify", {
+    params: {state: strategyRes.data.state}
+  });
+}
