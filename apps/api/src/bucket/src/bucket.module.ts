2f53885f8af197873612b7b81ea754e7e7dab125
Mavliut Fozliev <mavliut.fozliev@gmail.com>
2025-06-18 15:20:44 +0300

    add acl property to bucket schema

--- apps/api/src/bucket/src/bucket.module.ts
+++ apps/api/src/bucket/src/bucket.module.ts
@@ -45,7 +45,8 @@ export class BucketModule {
         "relationType",
         "dependent",
         // location
-        "locationType"
+        "locationType",
+        "acl"
       ]
     });
     const imports: (Type<any> | DynamicModule)[] = [
