8eae8f3e7f49a3a6fcaad2b9388a3d0cb213aeae
limonatasuyu <emredilek6@gmail.com>
2025-07-04 10:55:41 +0300

    feat: Implement Table UI Based on Figma Design

---
+++ apps/panel/src/components/organisms/bucket-table/BucketTable.tsx
@@ -0,0 +1,225 @@
+import {Button, Checkbox, Icon, type IconName} from "oziko-ui-kit";
+import Table from "../table/Table";
+import styles from "./BucketTable.module.scss";
+import {memo, useMemo} from "react";
+
+type FieldType =
+  | "string"
+  | "number"
+  | "date"
+  | "boolean"
+  | "textarea"
+  | "multiple selection"
+  | "relation"
+  | "location"
+  | "array"
+  | "object"
+  | "file"
+  | "richtext";
+
+export type ColumnType = {
+  header: any;
+  key: string;
+  type?: FieldType;
+  width?: string;
+  deletable?: boolean;
+  headerClassName?: string;
+  cellClassName?: string;
+  columnClassName?: string;
+  showDropdownIcon?: boolean;
+};
+
+type BucketTableProps = {
+  data: any[];
+  columns: ColumnType[];
+};
+
+type ColumnHeaderProps = {
+  title: string;
+  icon?: IconName;
+  showDropdownIcon?: boolean;
+};
+
+const ColumnHeader = ({title, icon, showDropdownIcon}: ColumnHeaderProps) => {
+  return (
+    <>
+      <div className={styles.columnHeaderText}>
+        {icon && <Icon name={icon} size="sm" className={styles.headerIcon} />}
+        {title}
+      </div>
+      {showDropdownIcon && (
+        <Button variant="icon">
+          <Icon name="chevronDown" size="lg" />
+        </Button>
+      )}
+    </>
+  );
+};
+
+// TODO: Refactor this function to render more appropriate UI elements for each field type.
+// Many field types are currently using the generic `renderDefault()`.
+function renderCell(cellData: any, type?: FieldType, deletable?: boolean) {
+  function renderDefault() {
+    return (
+      <>
+        {cellData}
+        {deletable && (
+          <Button variant="icon">
+            <Icon name="close" size="sm" />
+          </Button>
+        )}
+      </>
+    );
+  }
+  switch (type) {
+    case "string":
+      return renderDefault();
+    case "number":
+      return renderDefault();
+    case "date":
+      return renderDefault();
+    case "boolean":
+      return <Checkbox className={styles.checkbox} />;
+    case "textarea":
+      return renderDefault();
+    case "multiple selection":
+      return (
+        <div className={styles.arrayCell}>
+          {cellData.map((_: any, index: number) => (
+            <Button variant="icon" className={styles.grayBox}>
+              {index + 1}
+            </Button>
+          ))}
+          <Button variant="icon" className={styles.grayBox}>
+            <Icon name="plus" size="xs" />
+          </Button>
+          {deletable && (
+            <Button variant="icon">
+              <Icon name="close" size="sm" />
+            </Button>
+          )}
+        </div>
+      );
+    case "relation":
+      return renderDefault();
+    case "location":
+      return (
+        <div className={styles.locationCell}>
+          <img src="/locationx.png" className={styles.locationImage} />
+          <div>{cellData}</div>
+        </div>
+      );
+    case "array":
+      return renderDefault();
+    case "object":
+      return <div className={styles.objectCell}>{JSON.stringify(cellData)}</div>;
+    case "file":
+      return (
+        <div className={styles.fileCell}>
+          <Icon name="imageMultiple" size="xs" />
+          <span>{cellData}</span>
+        </div>
+      );
+    case "richtext":
+      return renderDefault();
+    default:
+      return cellData ? JSON.stringify(cellData) : <div />;
+  }
+}
+
+// TODO: Update the icon mappings below to use appropriate icons for each field type.
+// Currently, many field types are using the same placeholder icon ("formatQuoteClose").
+const COLUMN_ICONS: Record<FieldType, IconName> = {
+  string: "formatQuoteClose",
+  number: "formatQuoteClose",
+  date: "formatQuoteClose",
+  boolean: "formatQuoteClose",
+  textarea: "formatQuoteClose",
+  "multiple selection": "formatListChecks",
+  relation: "formatQuoteClose",
+  location: "mapMarker",
+  array: "formatQuoteClose",
+  object: "dataObject",
+  file: "imageMultiple",
+  richtext: "formatQuoteClose"
+};
+
+const defaultColumns: ColumnType[] = [
+  {
+    header: <ColumnHeader title={String.fromCharCode(160)} />,
+    key: "select",
+    type: "boolean",
+    width: "10px",
+    columnClassName: styles.selectColumn,
+    cellClassName: styles.selectCell
+  },
+  {
+    header: (
+      <Button variant="icon" className={styles.columnHeaderText}>
+        <Icon name={"plus"} size="sm" className={styles.headerIcon} />
+        New&nbsp;Field
+      </Button>
+    ),
+    key: "new field",
+    width: "10px",
+    cellClassName: styles.newFieldCell
+  }
+];
+
+const BucketTable = ({data, columns}: BucketTableProps) => {
+  const formattedColumns = useMemo(
+    () => [
+      defaultColumns[0],
+      ...columns.map(i => ({
+        ...i,
+        header: (
+          <ColumnHeader
+            title={i.header}
+            icon={i.type && COLUMN_ICONS[i.type]}
+            showDropdownIcon={i.showDropdownIcon}
+          />
+        )
+      })),
+      defaultColumns[1]
+    ],
+    [columns]
+  );
+
+  const columnMap = useMemo(() => {
+    return Object.fromEntries(
+      formattedColumns.map(col => [col.key, {type: col.type, deletable: col.deletable}])
+    );
+  }, [formattedColumns]);
+
+  const formattedData = useMemo(() => {
+    return data.map(item => {
+      const fullRow = {
+        select: "",
+        ...item,
+        "new field": ""
+      };
+
+      return Object.fromEntries(
+        Object.entries(fullRow).map(([key, value]) => {
+          const meta = columnMap[key];
+          return [key, renderCell(value, meta?.type, meta?.deletable)];
+        })
+      );
+    });
+  }, [data, columnMap]);
+
+  return (
+    <>
+      <Table
+        className={styles.table}
+        columns={formattedColumns.map(i => ({
+          ...i,
+          headerClassName: `${i.headerClassName} ${styles.columnHeader}`
+        }))}
+        data={formattedData}
+      />
+    </>
+  );
+};
+
+export default memo(BucketTable);
--- apps/panel/src/pages/home/Home.tsx
+++ apps/panel/src/pages/home/Home.tsx
@@ -1,10 +1,113 @@
-import React, { memo } from "react";
+import React, {memo} from "react";
 import styles from "./Home.module.scss";
-import { FlexElement } from "oziko-ui-kit";
+import {FlexElement} from "oziko-ui-kit";
 import "oziko-ui-kit/dist/index.css";
 import VideoDisplay from "../../components/molecules/video-display/VideoDisplay";
 import Quicklinks from "../../components/molecules/quicklinks/Quicklinks";
 import WelcomeText from "../../components/atoms/welcome-text/WelcomeText";
+import BucketTable, {type ColumnType} from "../../components/organisms/bucket-table/BucketTable";
+
+const mockData = [
+  {
+    id: "1",
+    name: "Homepage",
+    slug: "homepage",
+    user: "john_doe",
+    meta_tags: {_id: "642438aab86243423", somein: "somein", someinelse: "someinelse"},
+    localization: "23.521, 20.113",
+    pages: ["about", "contact"],
+    images: "hero.jpg"
+  },
+  {
+    id: "2",
+    name: "Blog",
+    slug: "blog",
+    user: "jane_smith",
+    meta_tags: {_id: "529769810329", somein: "somein", someinelse: "someinelse"},
+    localization: "23.521, 20.113",
+    pages: ["post-1", "post-2"],
+    images: "blog-banner.png"
+  },
+  {
+    id: "3",
+    name: "Store",
+    slug: "store",
+    user: "admin",
+    meta_tags: {_id: "529769810329", somein: "somein", someinelse: "someinelse"},
+    localization: "23.521, 20.113",
+    pages: ["product-1", "product-2"],
+    images: "storefront.png"
+  },
+  {
+    id: "4",
+    name: "Support",
+    slug: "support",
+    user: "support_user",
+    meta_tags: {_id: "529769810329", somein: "somein", someinelse: "someinelse"},
+    localization: "23.521, 20.113",
+    pages: ["faq", "contact-support"],
+    images: "support.png"
+  }
+];
+
+const columns = [
+  {
+    header: "_id",
+    key: "id",
+    width: "10px"
+  },
+  {
+    header: "name",
+    key: "name",
+    width: "10px",
+    type: "string",
+    deletable: true,
+    showDropdownIcon: true
+  },
+  {
+    header: "slug",
+    key: "slug",
+    width: "10px",
+    type: "string",
+    deletable: true,
+    showDropdownIcon: true
+  },
+  {
+    header: "user",
+    key: "user",
+    width: "10px",
+    type: "string",
+    showDropdownIcon: true
+  },
+  {
+    header: "meta_tags",
+    key: "meta_tags",
+    width: "10px",
+    type: "object",
+    showDropdownIcon: true
+  },
+  {
+    header: "localization",
+    key: "localization",
+    width: "10px",
+    type: "location",
+    showDropdownIcon: true
+  },
+  {
+    header: "pages",
+    key: "pages",
+    width: "10px",
+    type: "multiple selection",
+    showDropdownIcon: true
+  },
+  {
+    header: "images",
+    key: "images",
+    width: "10px",
+    type: "file",
+    showDropdownIcon: true
+  }
+];
 
 const Home = () => {
   return (
@@ -16,6 +119,7 @@ const Home = () => {
           <VideoDisplay />
         </FlexElement>
       </FlexElement>
+      <BucketTable columns={columns as ColumnType[]} data={mockData} />
     </div>
   );
 };
