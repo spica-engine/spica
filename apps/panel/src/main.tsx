77349c5fe79c137d825cc3ebd1435de7a0cf2d05
limonatasuyu <emredilek6@gmail.com>
2025-07-10 10:34:24 +0300

    feat: add BucketProvider to project root

--- apps/panel/src/main.tsx
+++ apps/panel/src/main.tsx
@@ -1,16 +1,19 @@
-import { StrictMode } from "react";
+import {StrictMode} from "react";
 import * as ReactDOM from "react-dom/client";
 import AppRouter from "./router";
 import "oziko-ui-kit/dist/index.css";
 import "./styles.scss";
-import { AuthProvider } from "./contexts/AuthContext";
+import {AuthProvider} from "./contexts/AuthContext";
+import { BucketProvider } from "./contexts/BucketContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
 root.render(
   <StrictMode>
-    <AuthProvider >
-      <AppRouter />
+    <AuthProvider>
+      <BucketProvider>
+        <AppRouter />
+      </BucketProvider>
     </AuthProvider>
   </StrictMode>
 );
