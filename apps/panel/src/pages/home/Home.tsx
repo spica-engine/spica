ceca176643ac5d23d4f2836112ea1e2924ab51fb
limonatasuyu <emredilek6@gmail.com>
2025-07-11 10:28:04 +0300

    chore: remove temporary BucketNavigatorPopup and test code from Home page

--- apps/panel/src/pages/home/Home.tsx
+++ apps/panel/src/pages/home/Home.tsx
@@ -1,17 +1,12 @@
-import React, {memo} from "react";
+import React, { memo } from "react";
 import styles from "./Home.module.scss";
-import {FlexElement} from "oziko-ui-kit";
+import { FlexElement } from "oziko-ui-kit";
 import "oziko-ui-kit/dist/index.css";
 import VideoDisplay from "../../components/molecules/video-display/VideoDisplay";
 import Quicklinks from "../../components/molecules/quicklinks/Quicklinks";
 import WelcomeText from "../../components/atoms/welcome-text/WelcomeText";
-import BucketNavigatorPopup from "../../components/molecules/bucket-navigator-popup/BucketNavigatorPopup";
-import {useBucket} from "../../contexts/BucketContext";
 
 const Home = () => {
-  const {buckets} = useBucket();
-  const myBucket = buckets?.[0];
-
   return (
     <div className={styles.container}>
       <FlexElement dimensionX="fill" direction="vertical" gap={10} className={styles.content}>
@@ -20,8 +15,6 @@ const Home = () => {
           <Quicklinks />
           <VideoDisplay />
         </FlexElement>
-        MyBucket: {JSON.stringify(myBucket)}
-        {myBucket && <BucketNavigatorPopup bucket={myBucket} />}
       </FlexElement>
     </div>
   );
