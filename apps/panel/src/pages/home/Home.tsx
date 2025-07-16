3fa66259929500304abf500db1fe9a6964b88a44
limonatasuyu <emredilek6@gmail.com>
2025-07-16 14:30:45 +0300

    chore: clean up Home component, remove temporary CategorySelectCreate test code

--- apps/panel/src/pages/home/Home.tsx
+++ apps/panel/src/pages/home/Home.tsx
@@ -1,15 +1,12 @@
-import React, {memo} from "react";
+import React, { memo } from "react";
 import styles from "./Home.module.scss";
-import {FlexElement} from "oziko-ui-kit";
+import { FlexElement } from "oziko-ui-kit";
 import "oziko-ui-kit/dist/index.css";
 import VideoDisplay from "../../components/molecules/video-display/VideoDisplay";
 import Quicklinks from "../../components/molecules/quicklinks/Quicklinks";
 import WelcomeText from "../../components/atoms/welcome-text/WelcomeText";
-import BucketNavigatorPopup from "../../components/molecules/bucket-navigator-popup/BucketNavigatorPopup";
-import {useBucket} from "../../contexts/BucketContext";
 
 const Home = () => {
-  const {buckets} = useBucket();
   return (
     <div className={styles.container}>
       <FlexElement dimensionX="fill" direction="vertical" gap={10} className={styles.content}>
@@ -19,13 +16,6 @@ const Home = () => {
           <VideoDisplay />
         </FlexElement>
       </FlexElement>
-      {buckets?.map(i => (
-        <div key={i._id} style={{border: "1px solid red"}}>
-          <div>Bucket ID: {i._id}</div>
-          <div>Data: {JSON.stringify(i)}</div>
-          <BucketNavigatorPopup bucket={i} />
-        </div>
-      ))}
     </div>
   );
 };
