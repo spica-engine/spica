13cb4d8985bd94bd63e90a0cd611dc80e59cf6ff
limonatasuyu <emredilek6@gmail.com>
2025-07-17 16:13:22 +0300

    feat: move login page to "/passport/identify" path and redirect to "/" path (home) if token exists (#1386)

--- apps/panel/src/router.tsx
+++ apps/panel/src/router.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, Navigate, RouterProvider} from "react-router-dom";
 import Login from "./pages/login/Login";
 import App from "./app/app";
 import Home from "./pages/home/Home";
@@ -7,18 +7,22 @@ import Layout from "./layout/Layout";
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/passport/identify",
     element: <Login />
   },
   {
     element: <Layout />,
     children: [
       {
+        index: true,
+        element: <Navigate to="dashboard" replace />
+      },
+      {
         path: "app",
         element: <App />
       },
       {
-        path: "home",
+        path: "dashboard",
         element: <Home />
       }
     ]
