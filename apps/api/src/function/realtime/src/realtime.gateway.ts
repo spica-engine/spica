20f776ec90784a84bf92944a9eb04dac21244ca2
Mehmet Ozan GÖL <125906748+Mozan0@users.noreply.github.com>
2025-07-18 16:50:37 +0300

    feat(api): listening realtime function schema changes  (#1394)

---
+++ apps/api/src/function/realtime/src/realtime.gateway.ts
@@ -0,0 +1,50 @@
+import {OnGatewayConnection, OnGatewayDisconnect, WebSocketGateway} from "@nestjs/websockets";
+import {RealtimeDatabaseService} from "@spica-server/database/realtime";
+import {GuardService} from "@spica-server/passport";
+import {getConnectionHandlers} from "@spica-server/realtime";
+
+@WebSocketGateway({
+  path: "/function"
+})
+export class RealtimeFunctionService implements OnGatewayConnection, OnGatewayDisconnect {
+  readonly COLLECTION = "function";
+
+  constructor(
+    private realtime: RealtimeDatabaseService,
+    private guardService: GuardService
+  ) {}
+
+  private handlers = getConnectionHandlers(
+    this.guardService,
+    async () => this.COLLECTION,
+    this.prepareOptions.bind(this),
+    error => ({
+      code: error.status || 500,
+      message: error.message || "Unexpected error"
+    }),
+    this.realtime
+  );
+
+  async handleConnection(client: WebSocket, req: any) {
+    return this.handlers.handleConnection(client, req);
+  }
+
+  async handleDisconnect(client: any) {
+    return this.handlers.handleDisconnect(client, client.upgradeReq);
+  }
+
+  async prepareOptions(client, req) {
+    const options: any = {};
+
+    if (req.query.has("filter")) {
+      try {
+        options.filter = JSON.parse(req.query.get("filter"));
+      } catch (e) {
+        client.send(JSON.stringify({code: 400, message: "Invalid filter JSON: " + e.message}));
+        return client.close(1003);
+      }
+    }
+
+    return options;
+  }
+}
