b87d4413983e3d282f5abd962bb081ff585b0bca
Mozan0 <mehmet.gol@std.antalya.edu.tr>
2025-08-06 13:22:20 +0300

    fix tests

--- apps/api/src/function/packages/identity/jest.config.ts
+++ apps/api/src/function/packages/identity/jest.config.ts
@@ -1,9 +1,22 @@
 const path = require("path");
 const {workspaceRoot} = require("@nx/devkit");
 
-export default {
+const commonConfig = {
   preset: "../../../../../../jest.preset.js",
   testEnvironment: "node",
-  coverageDirectory: path.join(workspaceRoot, "coverage/apps/api/function/packages/identity"),
-  testMatch: ["<rootDir>/test/**/*.spec.ts"]
+  coverageDirectory: path.join(workspaceRoot, "coverage/apps/api/function/packages/identity")
+};
+
+export default {
+  projects: [
+    {
+      ...commonConfig,
+      testMatch: ["<rootDir>/test/identity.spec.ts"],
+      setupFilesAfterEnv: [path.join(workspaceRoot, "jest.flaky.setup.js")]
+    },
+    {
+      ...commonConfig,
+      testMatch: ["<rootDir>/test/**/*.spec.ts"]
+    }
+  ]
 };
