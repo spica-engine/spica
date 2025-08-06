dfbf3a8a3d3535c19b32b43499102ff592e2c4f8
Mozan0 <mehmet.gol@std.antalya.edu.tr>
2025-08-06 14:11:17 +0300

    fix identity flaky test setup

--- apps/api/src/function/packages/identity/jest.config.ts
+++ apps/api/src/function/packages/identity/jest.config.ts
@@ -1,22 +1,9 @@
 const path = require("path");
 const {workspaceRoot} = require("@nx/devkit");
 
-const commonConfig = {
+export default {
   preset: "../../../../../../jest.preset.js",
   testEnvironment: "node",
-  coverageDirectory: path.join(workspaceRoot, "coverage/apps/api/function/packages/identity")
-};
-
-export default {
-  projects: [
-    {
-      ...commonConfig,
-      testMatch: ["<rootDir>/test/identity.spec.ts"],
-      setupFilesAfterEnv: [path.join(workspaceRoot, "jest.flaky.setup.js")]
-    },
-    {
-      ...commonConfig,
-      testMatch: ["<rootDir>/test/**/*.spec.ts"]
-    }
-  ]
+  coverageDirectory: path.join(workspaceRoot, "coverage/apps/api/function/packages/identity"),
+  testMatch: ["<rootDir>/test/**/*.spec.ts"]
 };
