d053cd8594bcc86913c5eb218372427486276d55
Mozan0 <mehmet.gol@std.antalya.edu.tr>
2025-08-27 14:12:59 +0300

    fix jest config

--- apps/api/src/mailer/jest.config.ts
+++ apps/api/src/mailer/jest.config.ts
@@ -5,5 +5,5 @@ export default {
   preset: "../../../../jest.preset.js",
   testEnvironment: "node",
   coverageDirectory: path.join(workspaceRoot, "coverage/apps/api/mailer"),
-  testMatch: ["<rootDir>/test/**/mailer.integration.spec.ts"]
+  testMatch: ["<rootDir>/test/**/*.spec.ts"]
 };
