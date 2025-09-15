b94adb76d2c59507d19064423a8e3b2f3d2a5616
limonatasuyu <emredilek6@gmail.com>
2025-09-15 16:03:35 +0300

    feat: add indeterminate state to "Display All" columns checkbox
    Improve the columns visibility UI by introducing an indeterminate state for the "Display All" checkbox, providing clearer feedback when only some columns are visible.

--- apps/panel/src/components/molecules/bucket-action-bar/BucketActionBar.tsx
+++ apps/panel/src/components/molecules/bucket-action-bar/BucketActionBar.tsx
@@ -67,6 +67,15 @@ const BucketActionBar = ({
     [debouncedSearch]
   );
 
+  const {allColumnsVisible, someColumnsVisible} = useMemo(() => {
+    const {_id, ...otherColumnsVisibility} = visibleColumns;
+    const otherColumnsValues = Object.values(otherColumnsVisibility);
+    const allColumnsVisible = otherColumnsValues.every(isVisible => isVisible);
+    const someColumnsVisible =
+      !allColumnsVisible && otherColumnsValues.some(isVisible => isVisible);
+    return {allColumnsVisible, someColumnsVisible};
+  }, [visibleColumns]);
+
   return (
     <div className={styles.container}>
       <SearchBar
@@ -85,7 +94,8 @@ const BucketActionBar = ({
           </Button>
         )}
         <Button
-          className={styles.refreshButton} variant="text"
+          className={styles.refreshButton}
+          variant="text"
           onClick={onRefresh}
           disabled={refreshLoading || searchLoading}
           loading={refreshLoading}
@@ -101,7 +111,8 @@ const BucketActionBar = ({
             <div>
               <Checkbox
                 label="Display All"
-                checked={Object.values(visibleColumns).every(v => v)}
+                checked={allColumnsVisible}
+                indeterminate={someColumnsVisible}
                 onChange={() => toggleColumn()}
                 className={styles.displayAllCheckbox}
               />
