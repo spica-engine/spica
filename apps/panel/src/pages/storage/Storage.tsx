3ad9355088f980ca320d3a0436ec770ef1103e3d
limonatasuyu <emredilek6@gmail.com>
2025-10-15 14:49:42 +0300

    feat(Storage): implement FilePreview component and refactor StorageItemColumns for improved file handling

--- apps/panel/src/pages/storage/Storage.tsx
+++ apps/panel/src/pages/storage/Storage.tsx
@@ -9,7 +9,7 @@ import {
 } from "oziko-ui-kit";
 import styles from "./Storage.module.scss";
 import {useGetStorageItemsQuery} from "../../store/api";
-import {useEffect, useState} from "react";
+import {memo, useEffect, useMemo, useRef, useState} from "react";
 import useStorage from "../../hooks/useStorage";
 import useFileView from "../../hooks/useFileView";
 import SearchBar from "../../components/atoms/search-bar/SearchBar";
@@ -18,16 +18,121 @@ import CreateFile from "../../components/molecules/create-file-modal/CreateFile"
 
 type TypeDirectories = [string, string?, string?];
 type TypeFiles = [TypeFile[]?, TypeFile[]?, TypeFile[]?];
+type TypeDirectoryDepth = 0 | 1 | 2
 
-const StorageItem = ({
-  item,
-  onFolderClick,
-  onFileClick
-}: {
+interface FilePreviewProps {
+  handleClosePreview: () => void;
+  previewFile?: TypeFile;
+}
+
+const FilePreview = memo(({handleClosePreview, previewFile}: FilePreviewProps) => {
+  const fileView = useFileView({file: previewFile});
+
+  return (
+    <FluidContainer
+      className={styles.filePreviewContent}
+      gap={10}
+      direction="vertical"
+      dimensionY="fill"
+      root={{
+        children: (
+          <FlexElement gap={10} direction="vertical">
+            <FluidContainer
+              dimensionX="fill"
+              alignment="rightCenter"
+              suffix={{
+                children: (
+                  <Button
+                    className={styles.closePreviewButton}
+                    variant="icon"
+                    onClick={handleClosePreview}
+                  >
+                    <Icon name="close" />
+                  </Button>
+                )
+              }}
+            />
+            <FlexElement className={styles.fileView}>{fileView}</FlexElement>
+          </FlexElement>
+        ),
+        className: styles.fileViewContainer
+      }}
+      suffix={{
+        className: styles.metadata,
+        children: (
+          <FlexElement direction="vertical" className={styles.metadataContent}>
+            <FlexElement direction="vertical" gap={10}>
+              <Text className={styles.metadataName}>{previewFile?.name}</Text>
+              <Text>{previewFile?.content?.type}</Text>
+              {/* The previewFile does not have no date value but the figma has a date in here */}
+              {(previewFile as any)?.createdAt && (
+                <Text>{new Date((previewFile as any).createdAt).toLocaleString()}</Text>
+              )}
+            </FlexElement>
+            <FlexElement gap={10}>
+              <Button className={styles.metadataButton} variant="icon">
+                <Icon name="folder" size={14} />
+                Copy
+              </Button>
+              <Button className={styles.metadataButton} variant="icon">
+                <Icon name="pencil" size={14} />
+                Edit
+              </Button>
+              <Button className={styles.metadataButton} variant="icon">
+                <Icon name="close" size={14} />
+                Replace
+              </Button>
+              <Button
+                className={`${styles.metadataButton} ${styles.metadataClearButton}`}
+                color="danger"
+              >
+                <Icon name="delete" size={14} />
+                Delete
+              </Button>
+            </FlexElement>
+          </FlexElement>
+        )
+      }}
+    />
+  );
+});
+
+interface StorageItemColumnProps {
+  files?: TypeFile[];
+  handleFolderClick: (folderName: string, depth: TypeDirectoryDepth) => void;
+  setPreviewFile: (file: TypeFile) => void;
+  depth: TypeDirectoryDepth;
+}
+
+const StorageItemColumn = memo(
+  ({files, handleFolderClick, setPreviewFile, depth}: StorageItemColumnProps) => {
+    return (
+      <FlexElement
+        className={styles.storageItemColumn}
+        direction="vertical"
+        alignment={"left" as TypeAlignment}
+        gap={10}
+      >
+        {files?.map((item, index) => (
+          <StorageItem
+            key={index}
+            item={item}
+            onFolderClick={folderName => handleFolderClick(folderName, depth)}
+            onFileClick={setPreviewFile}
+          />
+        ))}
+      </FlexElement>
+    );
+  }
+);
+
+interface StorageItemProps {
   item: TypeFile;
   onFolderClick?: (folderName: string) => void;
   onFileClick: (file: TypeFile) => void;
-}) => {
+}
+
+const StorageItem = memo(({item, onFolderClick, onFileClick}: StorageItemProps) => {
   const isFolder = item?.content?.type === "inode/directory";
   const handleFolderClick = () => onFolderClick?.(item.name);
   const handleFileClick = () => onFileClick(item);
@@ -42,30 +147,131 @@ const StorageItem = ({
       </Text>
     </FlexElement>
   );
-};
+});
+
+interface ActionButtonsProps {
+  directory: TypeDirectories;
+  files: TypeFiles;
+}
+
+const ActionButtons = memo(({directory, files}: ActionButtonsProps) => {
+  const currentPrefix = directory.slice(1).join("");
+  const currentItemNames = files
+    .map((filesArray, index) =>
+      filesArray?.map(f => {
+        return `${index === 0 ? "" : directory[index]}${f.name}`;
+      })
+    )
+    .flat()
+    .filter(Boolean) as string[];
+
+  return (
+    <FlexElement>
+      <Button className={styles.actionBarButton} variant="filled">
+        <Icon name="sort" />
+        Sort
+      </Button>
+      <Button className={styles.actionBarButton} variant="filled">
+        <Icon name="refresh" />
+        Refresh
+      </Button>
+      <CreateFile prefix={currentPrefix}>
+        {({onOpen}) => (
+          <Button className={styles.actionBarButton} variant="filled" onClick={onOpen}>
+            <Icon name="plus" />
+            Upload Files
+          </Button>
+        )}
+      </CreateFile>
+      <CreateFolder prefix={currentPrefix} currentItemNames={currentItemNames}>
+        {({onOpen}) => (
+          <Button className={styles.actionBarButton} variant="filled" onClick={onOpen}>
+            <Icon name="plus" />
+            Create New Folder
+          </Button>
+        )}
+      </CreateFolder>
+    </FlexElement>
+  );
+});
+
+interface StorageItemColumnsProps {
+  files: TypeFiles;
+  handleFolderClick: (folderName: string, depth: TypeDirectoryDepth) => void;
+  setPreviewFile: (file: TypeFile) => void;
+  directory: TypeDirectories;
+}
+
+const StorageItemColumns = memo(
+  ({files, handleFolderClick, setPreviewFile, directory}: StorageItemColumnsProps) => {
+    const renderSecondRow = directory[1] !== undefined;
+    const renderThirdRow = directory[2] !== undefined;
+
+    return (
+      <FluidContainer
+        dimensionY="fill"
+        dimensionX="fill"
+        prefix={{
+          className: styles.storageItemColumnContainer,
+          children: (
+            <StorageItemColumn
+              files={files[0]}
+              handleFolderClick={handleFolderClick}
+              setPreviewFile={setPreviewFile}
+              depth={0}
+            />
+          )
+        }}
+        root={{
+          className: `${styles.storageItemColumnContainer} ${renderSecondRow ? "" : styles.hidden}`,
+          children: (
+            <StorageItemColumn
+              files={files[1]}
+              handleFolderClick={handleFolderClick}
+              setPreviewFile={setPreviewFile}
+              depth={1}
+            />
+          )
+        }}
+        suffix={{
+          className: `${styles.storageItemColumnContainer} ${renderThirdRow ? "" : styles.hidden}`,
+          children: (
+            <StorageItemColumn
+              files={files[2]}
+              handleFolderClick={handleFolderClick}
+              setPreviewFile={setPreviewFile}
+              depth={2}
+            />
+          )
+        }}
+      />
+    );
+  }
+);
 
 export default function StoragePage() {
   const [directory, setDirectory] = useState<TypeDirectories>(["/"]);
+  const targetColumnIndex = useRef(0);
   const {buildDirectoryFilter, convertStorageToTypeFile} = useStorage();
-
-  const filter = buildDirectoryFilter(directory as string[]);
-
-  const {data: storageData, isLoading} = useGetStorageItemsQuery({filter});
+  const filter = useMemo(() => buildDirectoryFilter(directory as string[]), [directory]);
+  const {data: storageData} = useGetStorageItemsQuery({filter});
 
   const [files, setFiles] = useState<TypeFiles>([]);
   const [previewFile, setPreviewFile] = useState<TypeFile>();
 
-  const handleFolderClick = (folderName: string, directoryDepth: number) => {
-    const newDirectories: TypeDirectories = [...directory];
-    newDirectories[directoryDepth + 1] = folderName;
-    if (directoryDepth + 2 < 3) {
-      newDirectories[directoryDepth + 2] = undefined;
+  const handleFolderClick = (folderName: string, directoryDepth: TypeDirectoryDepth) => {
+    let newDirectories: TypeDirectories = [...directory];
+    if (directoryDepth === 0) {
+      newDirectories[2] = undefined;
+    } else if (directoryDepth === 2) {
+      newDirectories = newDirectories.slice(-3) as TypeDirectories;
+      const newFiles = [files[1], files[2]];
+      setFiles(newFiles as TypeFiles);
     }
+    newDirectories[directoryDepth + 1] = folderName;
+    targetColumnIndex.current = directoryDepth + 1;
     setDirectory(newDirectories);
-    setPreviewFile(undefined);
   };
-
-  const targetColumnIndex = directory[2] !== undefined ? 2 : directory[1] !== undefined ? 1 : 0;
 
   useEffect(() => {
     const convertedData = (storageData?.data ?? (storageData as unknown as TypeFile[]))?.map(
@@ -76,40 +282,24 @@ export default function StoragePage() {
 
     const transformedData = convertedData.map(i => {
       const isFolder = i.content?.type === "inode/directory";
-      const isInRoot = directory.length === 1;
+      const isInRoot = targetColumnIndex.current === 0;
       const nameParts = i.name.split("/").filter(Boolean);
       const resolvedName = nameParts[nameParts.length - 1] + (isFolder ? "/" : "");
       if (isFolder || !isInRoot) return {...i, name: resolvedName};
       return i;
     });
 
-    if (JSON.stringify(files[targetColumnIndex - 1]) === JSON.stringify(transformedData)) return;
     const newFiles: TypeFiles = [...files];
-    newFiles[targetColumnIndex] = transformedData;
+    newFiles[targetColumnIndex.current] = transformedData;
 
-    for (let i = targetColumnIndex + 1; i < 3; i++) {
+    for (let i = targetColumnIndex.current + 1; i < 3; i++) {
       newFiles[i] = undefined;
     }
 
     setFiles(newFiles);
-  }, [storageData, directory, convertStorageToTypeFile]);
-
-  const renderSecondRow = directory[1] !== undefined;
-  const renderThirdRow = directory[2] !== undefined;
-
-  const fileView = useFileView({file: previewFile});
+  }, [storageData]);
 
   const handleClosePreview = () => setPreviewFile(undefined);
-
-  const currentPrefix = directory.slice(1).join("");
-
-  const currentItemNames = files
-    .map((filesArray, index) =>
-      filesArray?.map(f => {
-        return `${index === 0 ? "" : directory[index]}${f.name}`;
-      })
-    )
-    .flat();
 
   return (
     <div className={styles.container}>
@@ -117,34 +307,7 @@ export default function StoragePage() {
         className={styles.actionBar}
         prefix={{children: <SearchBar />}}
         suffix={{
-          children: (
-            <FlexElement>
-              <Button className={styles.actionBarButton} variant="filled">
-                <Icon name="sort" />
-                Sort
-              </Button>
-              <Button className={styles.actionBarButton} variant="filled">
-                <Icon name="refresh" />
-                Refresh
-              </Button>
-              <CreateFile prefix={currentPrefix}>
-                {({onOpen}) => (
-                  <Button className={styles.actionBarButton} variant="filled" onClick={onOpen}>
-                    <Icon name="plus" />
-                    Upload Files
-                  </Button>
-                )}
-              </CreateFile>
-              <CreateFolder prefix={currentPrefix} currentItemNames={currentItemNames}>
-                {({onOpen}) => (
-                  <Button className={styles.actionBarButton} variant="filled" onClick={onOpen}>
-                    <Icon name="plus" />
-                    Create New Folder
-                  </Button>
-                )}
-              </CreateFolder>
-            </FlexElement>
-          )
+          children: <ActionButtons directory={directory} files={files} />
         }}
       />
       <FluidContainer
@@ -152,74 +315,11 @@ export default function StoragePage() {
         root={{
           className: styles.storageItemColumns,
           children: (
-            <FluidContainer
-              dimensionY="fill"
-              dimensionX="fill"
-              prefix={{
-                className: styles.storageItemColumnContainer,
-                children: (
-                  <FlexElement
-                    className={styles.storageItemColumn}
-                    direction="vertical"
-                    alignment={"left" as TypeAlignment}
-                    gap={10}
-                  >
-                    {files[0]?.map((item, index) => (
-                      <StorageItem
-                        key={index}
-                        item={item}
-                        onFolderClick={folderName => handleFolderClick(folderName, 0)}
-                        onFileClick={setPreviewFile}
-                      />
-                    ))}
-                  </FlexElement>
-                )
-              }}
-              root={
-                renderSecondRow
-                  ? {
-                      className: styles.storageItemColumnContainer,
-                      children: (
-                        <FlexElement
-                          className={styles.storageItemColumn}
-                          direction="vertical"
-                          alignment={"left" as TypeAlignment}
-                          gap={10}
-                        >
-                          {!isLoading &&
-                            files[1]?.map((item, index) => (
-                              <StorageItem
-                                key={index}
-                                item={item}
-                                onFolderClick={folderName => handleFolderClick(folderName, 1)}
-                                onFileClick={setPreviewFile}
-                              />
-                            ))}
-                        </FlexElement>
-                      )
-                    }
-                  : undefined
-              }
-              suffix={
-                renderThirdRow
-                  ? {
-                      className: styles.storageItemColumnContainer,
-                      children: (
-                        <FlexElement
-                          className={styles.storageItemColumn}
-                          direction="vertical"
-                          alignment={"left" as TypeAlignment}
-                          gap={10}
-                        >
-                          {!isLoading &&
-                            files[2]?.map((item, index) => (
-                              <StorageItem key={index} item={item} onFileClick={setPreviewFile} />
-                            ))}
-                        </FlexElement>
-                      )
-                    }
-                  : undefined
-              }
+            <StorageItemColumns
+              files={files}
+              handleFolderClick={handleFolderClick}
+              setPreviewFile={setPreviewFile}
+              directory={directory}
             />
           )
         }}
@@ -227,71 +327,7 @@ export default function StoragePage() {
           previewFile && {
             className: styles.preview,
             children: (
-              <FluidContainer
-                className={styles.filePreviewContent}
-                gap={10}
-                direction="vertical"
-                dimensionY="fill"
-                root={{
-                  children: (
-                    <FlexElement gap={10} direction="vertical">
-                      <FluidContainer
-                        dimensionX="fill"
-                        alignment="rightCenter"
-                        suffix={{
-                          children: (
-                            <Button
-                              className={styles.closePreviewButton}
-                              variant="icon"
-                              onClick={handleClosePreview}
-                            >
-                              <Icon name="close" />
-                            </Button>
-                          )
-                        }}
-                      />
-                      <FlexElement className={styles.fileView}>{fileView}</FlexElement>
-                    </FlexElement>
-                  ),
-                  className: styles.fileViewContainer
-                }}
-                suffix={{
-                  className: styles.metadata,
-                  children: (
-                    <FlexElement direction="vertical" className={styles.metadataContent}>
-                      <FlexElement direction="vertical" gap={10}>
-                        <Text className={styles.metadataName}>{previewFile?.name}</Text>
-                        <Text>{previewFile?.content?.type}</Text>
-                        {/* The previewFile does not have no date value but the figma has a date in here */}
-                        {(previewFile as any)?.createdAt && (
-                          <Text>{new Date((previewFile as any).createdAt).toLocaleString()}</Text>
-                        )}
-                      </FlexElement>
-                      <FlexElement gap={10}>
-                        <Button className={styles.metadataButton} variant="icon">
-                          <Icon name="folder" size={14} />
-                          Copy
-                        </Button>
-                        <Button className={styles.metadataButton} variant="icon">
-                          <Icon name="pencil" size={14} />
-                          Edit
-                        </Button>
-                        <Button className={styles.metadataButton} variant="icon">
-                          <Icon name="close" size={14} />
-                          Replace
-                        </Button>
-                        <Button
-                          className={`${styles.metadataButton} ${styles.metadataClearButton}`}
-                          color="danger"
-                        >
-                          <Icon name="delete" size={14} />
-                          Delete
-                        </Button>
-                      </FlexElement>
-                    </FlexElement>
-                  )
-                }}
-              />
+              <FilePreview handleClosePreview={handleClosePreview} previewFile={previewFile} />
             )
           }
         }
