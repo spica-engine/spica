43485044123a8e1e715bbeccfb998fb231f275eb
limonatasuyu <emredilek6@gmail.com>
2025-10-19 14:38:35 +0300

    feat(storage): add detailedMenu button functionalities and remove unrelated changes

--- apps/panel/src/components/molecules/storage-action-bar/StorageActionBar.tsx
+++ apps/panel/src/components/molecules/storage-action-bar/StorageActionBar.tsx
@@ -1,26 +1,8 @@
 import {FlexElement, FluidContainer, Icon, Button, Popover} from "oziko-ui-kit";
 import SearchBar from "../../atoms/search-bar/SearchBar";
 import styles from "./StorageActionBar.module.scss";
-import CreateFile from "../create-file-modal/CreateFile";
-import CreateFolder from "../create-folder-modal/CreateFolderModal";
-import type {TypeDirectories} from "src/components/organisms/storage-columns/StorageColumns";
-import {findMaxDepthDirectory, ROOT_PATH} from "../../../pages/storage/StorageHooks";
 
-interface StorageActionBarProps {
-  directory: TypeDirectories;
-}
-
-export default function StorageActionBar({directory}: StorageActionBarProps) {
-  const visibleDirectories = directory.filter(dir => dir.currentDepth);
-  const currentItemNames = visibleDirectories
-    .map(dir => dir.items?.map(item => item.name).filter(Boolean) || [])
-    .flat();
-
-  const deepestPath = findMaxDepthDirectory(directory)?.fullPath;
-  const prefix =
-    !deepestPath || deepestPath === ROOT_PATH
-      ? ""
-      : deepestPath.split("/").filter(Boolean).join("/") + "/";
+export default function StorageActionBar() {
 
   return (
     <FluidContainer
@@ -39,22 +21,14 @@ export default function StorageActionBar({directory}: StorageActionBarProps) {
               <Icon name="refresh" />
               Refresh
             </Button>
-            <CreateFile prefix={prefix}>
-              {({onOpen}) => (
-                <Button className={styles.actionBarButton} variant="filled" onClick={onOpen}>
-                  <Icon name="plus" />
-                  Upload Files
-                </Button>
-              )}
-            </CreateFile>
-            <CreateFolder prefix={prefix} currentItemNames={currentItemNames}>
-              {({onOpen}) => (
-                <Button className={styles.actionBarButton} variant="filled" onClick={onOpen}>
-                  <Icon name="plus" />
-                  Create New Folder
-                </Button>
-              )}
-            </CreateFolder>
+            <Button className={styles.actionBarButton} variant="filled">
+              <Icon name="plus" />
+              Upload Files
+            </Button>
+            <Button className={styles.actionBarButton} variant="filled">
+              <Icon name="plus" />
+              Create New Folder
+            </Button>
           </FlexElement>
         )
       }}
--- apps/panel/src/pages/storage/Storage.tsx
+++ apps/panel/src/pages/storage/Storage.tsx
@@ -1,4 +1,4 @@
-import {FluidContainer} from "oziko-ui-kit";
+import {FluidContainer, type TypeFile} from "oziko-ui-kit";
 import styles from "./Storage.module.scss";
 import {StorageItemColumns} from "../../components/organisms/storage-columns/StorageColumns";
 import {FilePreview} from "../../components/molecules/file-preview/FilePreview";
@@ -15,7 +15,7 @@ export default function StoragePage() {
 
   const {previewFile, setPreviewFile, handleClosePreview} = useFilePreview();
   useStorageDataSync(directory, setDirectory);
-  const {onUploadComplete} = useFileOperations(directory, setDirectory, setPreviewFile);
+  const {onUploadComplete, onFileReplaced, onFileDeleted} = useFileOperations(directory, setDirectory, setPreviewFile);
 
   const handleFolderClick = (
     folderName: string,
@@ -27,41 +27,9 @@ export default function StoragePage() {
     onFolderClick(folderName, fullPath, directoryDepth, wasActive);
   };
 
-  const onFileReplaced = (updatedFile: TypeFile) => {
-    const newDirectories = directory.map(dir => {
-      if (dir.items) {
-        const updatedItems = dir.items.map(item =>
-          item._id === updatedFile._id ? updatedFile : item
-        );
-        return {
-          ...dir,
-          items: updatedItems
-        };
-      }
-      return dir;
-    });
-    setDirectory(newDirectories as TypeDirectories);
-    setPreviewFile(updatedFile as DirectoryItem);
-  };
-
-  const onFileDeleted = (fileId: string) => {
-    const newDirectories = directory.map(dir => {
-      if (dir.items) {
-        const filteredItems = dir.items.filter(item => item._id !== fileId);
-        return {
-          ...dir,
-          items: filteredItems
-        };
-      }
-      return dir;
-    });
-    setDirectory(newDirectories as TypeDirectories);
-    setPreviewFile(undefined);
-  };
-
   return (
     <div className={styles.container}>
-      <StorageActionBar directory={directory} />
+      <StorageActionBar />
       <FluidContainer
         className={styles.storageItemContainer}
         root={{
