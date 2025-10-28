d8e87b0ec47c8beb4cf6462f2517f5bddb051046
limonatasuyu <emredilek6@gmail.com>
2025-10-28 14:50:08 +0300

    refactor(Storage): Make sure data converted correctly

--- apps/panel/src/pages/storage/hooks/useStorageConverter.tsx
+++ apps/panel/src/pages/storage/hooks/useStorageConverter.tsx
@@ -1,7 +1,6 @@
 import type {DirectoryItem, TypeDirectories} from "../../../types/storage";
 import type {Storage} from "../../../store/api/storageApi";
 import {findMaxDepthDirectory} from "../utils";
-import {ROOT_PATH} from "../constants";
 import useStorageService from "../../../hooks/useStorage";
 import {useCallback} from "react";
 
@@ -15,26 +14,19 @@ export function useStorageConverter(directory: TypeDirectories) {
       const dirToConvert = findMaxDepthDirectory(directory);
       if (!dirToConvert) return undefined;
 
-      const parentPath =
-        dirToConvert.fullPath === ROOT_PATH
-          ? ""
-          : dirToConvert.fullPath.split("/").filter(Boolean).join("/") + "/";
-
       return data.map(storage => {
         const typeFile = convertStorageToTypeFile(storage);
-        const fullPath = typeFile.name;
         const isDirectory = typeFile.content?.type === "inode/directory";
-      const nameParts = typeFile.name.split("/").filter(Boolean);
-      const resolvedName = nameParts[nameParts.length - 1] + (isDirectory ? "/" : "");
+        const nameParts = typeFile.name.split("/").filter(Boolean);
+        const resolvedName = nameParts[nameParts.length - 1] + (isDirectory ? "/" : "");
 
         return {
-          _id: typeFile._id,
-          name: resolvedName,
-          fullPath: fullPath,
-          content: typeFile.content,
-          url: typeFile.url,
-          parentPath,
-          isDirectory
+          ...typeFile,
+          items: undefined,
+          label: resolvedName,
+          fullPath: storage.name,
+          currentDepth: directory.filter(dir => dir.currentDepth).length,
+          isActive: false
         } as DirectoryItem;
       });
     },
