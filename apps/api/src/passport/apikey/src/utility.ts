7ce536a01b2312d8ccc2ab01d78ea870abe2d9ec
Mozan0 <mehmet.gol@std.antalya.edu.tr>
2025-10-30 17:23:21 +0300

    change request

--- apps/api/src/passport/apikey/src/utility.ts
+++ apps/api/src/passport/apikey/src/utility.ts
@@ -1,15 +1,7 @@
 import {ApiKeyService} from "./apikey.service";
-import {customAlphabet} from "nanoid";
 
 export function providePolicyFinalizer(apikeyService: ApiKeyService) {
   return (policyId: string) => {
     return apikeyService.updateMany({policies: {$in: [policyId]}}, {$pull: {policies: policyId}});
   };
-}
-
-export function generateUniqueApiKey() {
-  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+";
-  const generateApiKey = customAlphabet(chars, 32);
-  const key = generateApiKey();
-  return key;
 }
