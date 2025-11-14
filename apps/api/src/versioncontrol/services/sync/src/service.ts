7ab6de635ef2e3607bdc06715cc8a6268dc6d8a0
Tuna <Spica>
2025-11-14 11:35:45 +0300

    ttl for sync service

--- apps/api/src/versioncontrol/services/sync/src/service.ts
+++ apps/api/src/versioncontrol/services/sync/src/service.ts
@@ -11,7 +11,7 @@ import {Sync} from "@spica-server/interface/versioncontrol";
 @Injectable()
 export class SyncService extends BaseCollection<Sync>("sync") {
   constructor(db: DatabaseService) {
-    super(db);
+    super(db, {afterInit: () => this.upsertTTLIndex(30 * 24 * 60 * 60)});
   }
 
   watch(
