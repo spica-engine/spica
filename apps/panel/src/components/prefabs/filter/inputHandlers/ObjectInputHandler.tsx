9bc3d804d432cfda878026a110933f240ead7923
Kenan Gasimov <kenangasimov@Mac.modem.turktelekom>
2025-12-29 17:29:49 +0300

    feat: make object filter to be closed via pressing enter

--- apps/panel/src/components/prefabs/filter/inputHandlers/ObjectInputHandler.tsx
+++ apps/panel/src/components/prefabs/filter/inputHandlers/ObjectInputHandler.tsx
@@ -3,13 +3,41 @@
  * email: rio.kenan@gmail.com
  */
 
-import React, {useState} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import type {FilterInputHandlerProps} from "./FilterInputHandlerRegistry";
 import {ObjectMinimizedInput} from "oziko-ui-kit";
 
 const ObjectInputHandler: React.FC<FilterInputHandlerProps> = ({value, onChange, property}) => {
   const [isOpen, setIsOpen] = useState(false);
 
+
+  const handleKeyDown = useCallback(
+    (e: KeyboardEvent) => {
+      const actions: Partial<Record<KeyboardEvent["key"], () => void>> = {
+        Enter: () => onChange(value),
+      };
+  
+      const action = actions[e.key];
+      if (!action) return;
+  
+      e.preventDefault();
+      e.stopPropagation();
+  
+      action();
+      setIsOpen(false);
+    },
+    [value, onChange]
+  );
+
+  useEffect(() => {
+    if (!isOpen) return;
+  
+    globalThis.addEventListener("keydown", handleKeyDown);
+  
+    return () => {
+      globalThis.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [isOpen, handleKeyDown]);
 
   return (
     <ObjectMinimizedInput
