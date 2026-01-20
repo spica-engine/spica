554f206c22afc838187281d30b0399446726f03e
Mozan0 <mehmet.gol@std.antalya.edu.tr>
2026-01-20 13:49:26 +0300

    remove user profile test implementations

--- apps/api/src/passport/user/jest.config.ts
+++ apps/api/src/passport/user/jest.config.ts
@@ -1,22 +1,9 @@
 const path = require("path");
 const {workspaceRoot} = require("@nx/devkit");
 
-const commonConfig = {
+export default {
   preset: "../../../../../jest.preset.js",
   testEnvironment: "node",
-  coverageDirectory: path.join(workspaceRoot, "coverage/apps/api/passport/user")
-};
-export default {
-  projects: [
-    {
-      ...commonConfig,
-      modulePathIgnorePatterns: ["<rootDir>/test/user.controller.spec.ts"],
-      testMatch: ["<rootDir>/test/*.spec.ts"]
-    },
-    {
-      ...commonConfig,
-      testMatch: ["<rootDir>/test/user.controller.spec.ts"],
-      setupFilesAfterEnv: [path.join(workspaceRoot, "jest.flaky.setup.js")]
-    }
-  ]
+  coverageDirectory: path.join(workspaceRoot, "coverage/apps/api/passport/user"),
+  testMatch: ["<rootDir>/test/**/*.spec.ts"]
 };
