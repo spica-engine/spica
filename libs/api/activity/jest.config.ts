409a0dfa0371965bc92c1334ec301e8a55b82161
Tuna <h.tuna96@gmail.com>
2026-03-30 17:54:12 +0300

    move activity

--- libs/api/activity/jest.config.ts
+++ libs/api/activity/jest.config.ts
@@ -2,8 +2,8 @@ const path = require("path");
 const {workspaceRoot} = require("@nx/devkit");
 
 export default {
-  preset: "../../../../jest.preset.js",
+  preset: "../../../jest.preset.js",
   testEnvironment: "node",
-  coverageDirectory: path.join(workspaceRoot, "coverage/apps/api/activity"),
+  coverageDirectory: path.join(workspaceRoot, "coverage/libs/api/activity"),
   testMatch: ["<rootDir>/test/**/*.spec.ts"]
 };
