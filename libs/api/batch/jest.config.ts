b26db89b5e91c2cf20520973f7a6f78e8863a7f0
Tuna <h.tuna96@gmail.com>
2026-03-30 17:26:45 +0300

    move api/batch

--- libs/api/batch/jest.config.ts
+++ libs/api/batch/jest.config.ts
@@ -2,8 +2,8 @@ const path = require("path");
 const {workspaceRoot} = require("@nx/devkit");
 
 export default {
-  preset: "../../../../jest.preset.js",
+  preset: "../../../jest.preset.js",
   testEnvironment: "node",
-  coverageDirectory: path.join(workspaceRoot, "coverage/apps/api/batch"),
+  coverageDirectory: path.join(workspaceRoot, "coverage/libs/api/batch"),
   testMatch: ["<rootDir>/test/**/*.spec.ts"]
 };
