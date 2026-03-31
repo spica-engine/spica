74975b1fa2fc80bc86ff4f4f1c86081bf7253ef5
Tuna <h.tuna96@gmail.com>
2026-03-31 16:20:22 +0300

    sms

--- libs/api/sms/jest.config.ts
+++ libs/api/sms/jest.config.ts
@@ -2,8 +2,8 @@ const path = require("path");
 const {workspaceRoot} = require("@nx/devkit");
 
 export default {
-  preset: "../../../../jest.preset.js",
+  preset: "../../../jest.preset.js",
   testEnvironment: "node",
-  coverageDirectory: path.join(workspaceRoot, "coverage/apps/api/sms"),
+  coverageDirectory: path.join(workspaceRoot, "coverage/libs/api/sms"),
   testMatch: ["<rootDir>/test/**/*.spec.ts"]
 };
