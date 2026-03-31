a844acff2ff39ed4534b23a1eda1738edf565633
Tuna <h.tuna96@gmail.com>
2026-03-31 16:44:57 +0300

    status

--- libs/api/status/jest.config.ts
+++ libs/api/status/jest.config.ts
@@ -2,9 +2,9 @@ const path = require("path");
 const {workspaceRoot} = require("@nx/devkit");
 
 export default {
-  preset: "../../../../../jest.preset.js",
+  preset: "../../../jest.preset.js",
   testEnvironment: "node",
-  coverageDirectory: path.join(workspaceRoot, "coverage/apps/api/status/services"),
-  testMatch: ["<rootDir>/**/*.spec.ts"],
+  coverageDirectory: path.join(workspaceRoot, "coverage/libs/api/status"),
+  testMatch: ["<rootDir>/test/**/*.spec.ts"],
   setupFilesAfterEnv: [path.join(workspaceRoot, "jest.flaky.setup.js")]
 };
--- libs/api/status/services/jest.config.ts
+++ libs/api/status/services/jest.config.ts
@@ -4,7 +4,7 @@ const {workspaceRoot} = require("@nx/devkit");
 export default {
   preset: "../../../../jest.preset.js",
   testEnvironment: "node",
-  coverageDirectory: path.join(workspaceRoot, "coverage/apps/api/status"),
-  testMatch: ["<rootDir>/test/**/*.spec.ts"],
+  coverageDirectory: path.join(workspaceRoot, "coverage/libs/api/status/services"),
+  testMatch: ["<rootDir>/**/*.spec.ts"],
   setupFilesAfterEnv: [path.join(workspaceRoot, "jest.flaky.setup.js")]
 };
